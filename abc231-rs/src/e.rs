9917c08cd3e80f9b1278628c0b801af5e93fd046
hiratasa <36002166+hiratasa@users.noreply.github.com>
2021-12-11 23:09:21 +0900

    Add codes

---
+++ abc231-rs/src/e.rs
@@ -0,0 +1,214 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+
+// vec with some initial value
+#[allow(unused_macros)]
+macro_rules! vvec {
+    ($($x:expr),+; $y:expr; $n:expr) => {{
+        let mut v = vec![$y; $n];
+
+        let mut it = v.iter_mut();
+        $(
+            *it.next().unwrap() = $x;
+        )+
+
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! it {
+    ($x:expr) => {
+        once($x)
+    };
+    ($first:expr,$($x:expr),+) => {
+        chain(
+            once($first),
+            it!($($x),+)
+        )
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        bs.buffer_mut()[0] = $x as u64;
+        bs
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! pushed {
+    ($c:expr, $x:expr) => {{
+        let mut c = $c;
+        c.push($x);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! inserted {
+    ($c:expr, $($x:expr),*) => {{
+        let mut c = $c;
+        c.insert($($x),*);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! read_tuple {
+    ($($t:ty),+) => {{
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let mut it = line.trim()
+            .split_whitespace();
+
+        ($(
+            it.next().unwrap().parse::<$t>().ok().unwrap()
+        ),+)
+    }}
+}
+
+#[allow(dead_code)]
+fn read<T: FromStr>() -> T {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    line.trim().to_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_str() -> Vec<char> {
+    read::<String>().chars().collect()
+}
+
+#[allow(dead_code)]
+fn read_row<T: FromStr>() -> Vec<T> {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    line.trim()
+        .split_whitespace()
+        .map(|s| s.parse().ok().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_col<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read()).collect()
+}
+
+#[allow(dead_code)]
+fn read_mat<T: FromStr>(n: usize) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_row()).collect()
+}
+
+#[allow(dead_code)]
+fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
+    (0..n).map(|_| f()).collect()
+}
+
+trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
+where
+    T: 'a + Copy,
+{
+    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
+        self.into_iter().copied()
+    }
+}
+
+impl<'a, T, I> IterCopyExt<'a, T> for I
+where
+    I: IntoIterator<Item = &'a T>,
+    T: 'a + Copy,
+{
+}
+
+#[allow(dead_code)]
+fn calc0(x: usize, a: &[usize]) -> usize {
+    a.citer()
+        .rev()
+        .scan(x, |xx, aa| {
+            let m = *xx / aa;
+            *xx %= aa;
+
+            Some(m)
+        })
+        .sum()
+}
+
+#[allow(dead_code)]
+fn solve0(x: usize, a: &[usize]) -> usize {
+    let n = a.len();
+
+    let s0 = calc0(x, a);
+
+    (x..a[n - 1] * s0)
+        .map(|y| calc0(y, a) + calc0(y - x, a))
+        .min()
+        .unwrap()
+}
+
+fn main() {
+    let (n, x) = read_tuple!(usize, usize);
+    let a = read_row::<usize>();
+
+    let mut b = a
+        .citer()
+        .rev()
+        .scan(x, |xx, aa| {
+            let m = *xx / aa;
+            *xx %= aa;
+
+            Some(m)
+        })
+        .collect::<Vec<_>>();
+    b.reverse();
+
+    let dp = (0..n).fold(vec![0; n + 1], |mut dp, i| {
+        dp[i + 1] = (0..=i)
+            .map(|j| {
+                let s = izip!(b[j..i].citer(), a[j..i].citer())
+                    .map(|(bb, aa)| bb * aa)
+                    .sum::<usize>();
+                let t = if s == 0 {
+                    b[i]
+                } else {
+                    b[i] + 1 + calc0(a[i] - s, &a)
+                };
+
+                dp[j] + t
+            })
+            .min()
+            .unwrap();
+
+        dp
+    });
+
+    let ans = dp[n];
+    println!("{}", ans);
+}
---
+++ abc231-rs/src/f.rs
@@ -0,0 +1,308 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+
+// vec with some initial value
+#[allow(unused_macros)]
+macro_rules! vvec {
+    ($($x:expr),+; $y:expr; $n:expr) => {{
+        let mut v = vec![$y; $n];
+
+        let mut it = v.iter_mut();
+        $(
+            *it.next().unwrap() = $x;
+        )+
+
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! it {
+    ($x:expr) => {
+        once($x)
+    };
+    ($first:expr,$($x:expr),+) => {
+        chain(
+            once($first),
+            it!($($x),+)
+        )
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        bs.buffer_mut()[0] = $x as u64;
+        bs
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! pushed {
+    ($c:expr, $x:expr) => {{
+        let mut c = $c;
+        c.push($x);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! inserted {
+    ($c:expr, $($x:expr),*) => {{
+        let mut c = $c;
+        c.insert($($x),*);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! read_tuple {
+    ($($t:ty),+) => {{
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let mut it = line.trim()
+            .split_whitespace();
+
+        ($(
+            it.next().unwrap().parse::<$t>().ok().unwrap()
+        ),+)
+    }}
+}
+
+#[allow(dead_code)]
+fn read<T: FromStr>() -> T {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    line.trim().to_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_str() -> Vec<char> {
+    read::<String>().chars().collect()
+}
+
+#[allow(dead_code)]
+fn read_row<T: FromStr>() -> Vec<T> {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    line.trim()
+        .split_whitespace()
+        .map(|s| s.parse().ok().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_col<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read()).collect()
+}
+
+#[allow(dead_code)]
+fn read_mat<T: FromStr>(n: usize) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_row()).collect()
+}
+
+#[allow(dead_code)]
+fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
+    (0..n).map(|_| f()).collect()
+}
+
+trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
+where
+    T: 'a + Copy,
+{
+    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
+        self.into_iter().copied()
+    }
+}
+
+impl<'a, T, I> IterCopyExt<'a, T> for I
+where
+    I: IntoIterator<Item = &'a T>,
+    T: 'a + Copy,
+{
+}
+
+trait Monoid {
+    type Item: Clone;
+
+    fn id() -> Self::Item;
+    fn op(lhs: &Self::Item, rhs: &Self::Item) -> Self::Item;
+}
+
+struct BIT<M>
+where
+    M: Monoid,
+{
+    len: usize,
+    values: Vec<M::Item>,
+}
+
+#[allow(dead_code)]
+impl<M> BIT<M>
+where
+    M: Monoid,
+{
+    fn new(len: usize) -> BIT<M> {
+        BIT {
+            len,
+            values: vec![M::id(); len],
+        }
+    }
+
+    fn with(vals: &Vec<M::Item>) -> Self {
+        let mut bit = Self::new(vals.len());
+
+        for (i, v) in vals.iter().enumerate() {
+            bit.add(i, v.clone());
+        }
+
+        bit
+    }
+
+    // [0, i)の和
+    fn sum(&self, i: usize) -> M::Item {
+        let mut s = M::id();
+        let mut idx = i as i64;
+
+        // values[1] ~ values[i] の和
+        // (bは1-indexedなのでこれでOK)
+        while idx > 0 {
+            s = M::op(&s, &self.values[(idx - 1) as usize]);
+            idx -= idx & -idx;
+        }
+
+        return s;
+    }
+
+    fn add(&mut self, i: usize, a: M::Item) {
+        // 1-indexedに直す
+        let mut idx = i as i64 + 1;
+
+        while idx as usize <= self.len {
+            self.values[(idx - 1) as usize] = M::op(&self.values[(idx - 1) as usize], &a);
+            idx += idx & -idx;
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+// 可換群
+trait Group: Monoid {
+    fn inv(value: &Self::Item) -> Self::Item;
+}
+
+#[allow(dead_code)]
+impl<M> BIT<M>
+where
+    M: Group,
+{
+    // [i, j) の和
+    fn sum_between(&self, i: usize, j: usize) -> M::Item {
+        M::op(&self.sum(j), &M::inv(&self.sum(i)))
+    }
+}
+
+macro_rules! define_monoid {
+    ($name: ident, $t: ty, $id: expr, $op: expr) => {
+        struct $name;
+
+        impl Monoid for $name {
+            type Item = $t;
+
+            fn id() -> Self::Item {
+                $id
+            }
+
+            fn op(lhs: &Self::Item, rhs: &Self::Item) -> Self::Item {
+                ($op)(*lhs, *rhs)
+            }
+        }
+    };
+}
+
+macro_rules! define_group {
+    ($name: ident, $t: ty, $id: expr, $op: expr, $inv: expr) => {
+        define_monoid!($name, $t, $id, $op);
+
+        impl Group for $name {
+            fn inv(value: &Self::Item) -> Self::Item {
+                ($inv)(*value)
+            }
+        }
+    };
+}
+
+define_group!(Sum, usize, 0, usize::wrapping_add, usize::wrapping_neg);
+
+fn main() {
+    let n: usize = read();
+    let a = read_row::<usize>();
+    let b = read_row::<usize>();
+
+    let a_values = a.citer().sorted().dedup().collect::<Vec<_>>();
+    let a_idxs = a_values
+        .citer()
+        .enumerate()
+        .map(|(i, aa)| (aa, i))
+        .collect::<FxHashMap<_, _>>();
+    let c = a.citer().map(|aa| a_idxs[&aa]).collect::<Vec<_>>();
+
+    let b_values = b.citer().sorted().dedup().collect::<Vec<_>>();
+    let b_idxs = b_values
+        .citer()
+        .enumerate()
+        .map(|(i, aa)| (aa, i))
+        .collect::<FxHashMap<_, _>>();
+    let d = b.citer().map(|aa| b_idxs[&aa]).collect::<Vec<_>>();
+
+    let cd = izip!(c, d)
+        .sorted_by_key(|&(c, d)| (c, Reverse(d)))
+        .collect::<Vec<_>>();
+
+    // eprintln!("{:?}", cd);
+    let ans = cd
+        .citer()
+        .group_by(|&x| x)
+        .into_iter()
+        .map(|(x, it)| (x, it.count()))
+        .scan(BIT::<Sum>::new(n), |bit, ((c, d), k)| {
+            let m = /* self */ k * k + k * bit.sum_between(d, n);
+
+            bit.add(d, k);
+
+            Some(m)
+        })
+        // .inspect(|t| eprintln!("{}", t))
+        .sum::<usize>();
+
+    println!("{}", ans);
+}
