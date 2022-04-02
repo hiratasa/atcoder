942fd02510383144c1fd147b05c5072cdc58dbc1
hiratasa <36002166+hiratasa@users.noreply.github.com>
2022-04-02 23:06:42 +0900

    Add codes

---
+++ abc246-rs/src/e.rs
@@ -0,0 +1,207 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+
+// vec with some initial value
+#[allow(unused_macros)]
+macro_rules! vvec {
+    ($($x:expr),+; $y:expr; $n:expr) => {{
+        let mut v = vec![$y; $n];
+
+        let mut it = v.iter_mut();
+        $(
+            *it.next().unwrap() = $x;
+        )+
+
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! it {
+    ($x:expr) => {
+        once($x)
+    };
+    ($first:expr,$($x:expr),+) => {
+        chain(
+            once($first),
+            it!($($x),+)
+        )
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        bs.buffer_mut()[0] = $x as u64;
+        bs
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! pushed {
+    ($c:expr, $x:expr) => {{
+        let x = $x;
+        let mut c = $c;
+        c.push(x);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! inserted {
+    ($c:expr, $($x:expr),*) => {{
+        let mut c = $c;
+        c.insert($($x),*);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! read_tuple {
+    ($($t:ty),+) => {{
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let mut it = line.trim()
+            .split_whitespace();
+
+        ($(
+            it.next().unwrap().parse::<$t>().ok().unwrap()
+        ),+)
+    }}
+}
+
+#[allow(dead_code)]
+fn read<T: FromStr>() -> T {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    line.trim().to_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_str() -> Vec<char> {
+    read::<String>().chars().collect()
+}
+
+#[allow(dead_code)]
+fn read_row<T: FromStr>() -> Vec<T> {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    line.trim()
+        .split_whitespace()
+        .map(|s| s.parse().ok().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_col<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read()).collect()
+}
+
+#[allow(dead_code)]
+fn read_mat<T: FromStr>(n: usize) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_row()).collect()
+}
+
+#[allow(dead_code)]
+fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
+    (0..n).map(|_| f()).collect()
+}
+
+trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
+where
+    T: 'a + Copy,
+{
+    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
+        self.into_iter().copied()
+    }
+}
+
+impl<'a, T, I> IterCopyExt<'a, T> for I
+where
+    I: IntoIterator<Item = &'a T>,
+    T: 'a + Copy,
+{
+}
+
+fn main() {
+    let n = read::<usize>();
+    let (ax, ay) = read_tuple!(usize, usize);
+    let (bx, by) = read_tuple!(usize, usize);
+
+    let s = read_vec(n, || read_str());
+
+    let ax = ax - 1;
+    let ay = ay - 1;
+    let bx = bx - 1;
+    let by = by - 1;
+
+    let mut q = VecDeque::new();
+    let mut costs = vec![vec![vec![usize::MAX; n]; n]; 4];
+
+    for i in 0..4 {
+        q.push_back((0, i, ax, ay));
+        costs[i][ax][ay] = 0;
+    }
+
+    let dirs = [
+        (usize::MAX, usize::MAX),
+        (usize::MAX, 1),
+        (1, usize::MAX),
+        (1, 1),
+    ];
+
+    while let Some((c, dir, x, y)) = q.pop_front() {
+        if c > costs[dir][x][y] {
+            continue;
+        }
+
+        if (x, y) == (bx, by) {
+            println!("{}", c + 1);
+            return;
+        }
+
+        (0..4).for_each(|dir2| {
+            if c + 1 < costs[dir2][x][y] {
+                q.push_back((c + 1, dir2, x, y));
+                costs[dir2][x][y] = c + 1;
+            }
+        });
+
+        let (di, dj) = dirs[dir];
+        let (ni, nj) = (x.wrapping_add(di), y.wrapping_add(dj));
+        if ni < n && nj < n && s[ni][nj] == '.' {
+            if c < costs[dir][ni][nj] {
+                q.push_front((c, dir, ni, nj));
+                costs[dir][ni][nj] = c;
+            }
+        }
+    }
+
+    println!("-1");
+}
