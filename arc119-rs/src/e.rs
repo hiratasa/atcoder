99911db2b78bb81f3542d808f43f72249196eed1
hiratasa <36002166+hiratasa@users.noreply.github.com>
2022-06-25 19:54:18 +0900

    Add codes

--- arc119-rs/src/e.rs
+++ arc119-rs/src/e.rs
@@ -148,4 +148,94 @@ where
 {
 }
 
-fn main() {}
+fn main() {
+    let n = read::<usize>();
+    let a = read_row::<i64>();
+
+    // 反転しない場合
+    let ans0 = a
+        .citer()
+        .tuple_windows()
+        .map(|(a0, a1)| (a0 - a1).abs())
+        .sum::<i64>();
+
+    // 端っこから反転する場合（両方）
+    let ans1 = (1..n)
+        .map(|i| -(a[i - 1] - a[i]).abs() + (a[0] - a[i]).abs())
+        .min()
+        .map_or(ans0, |x| ans0 + x);
+    let ans2 = (1..n)
+        .map(|i| -(a[i - 1] - a[i]).abs() + (a[i - 1] - a[n - 1]).abs())
+        .min()
+        .map_or(ans0, |x| ans0 + x);
+
+    // 途中を反転する場合
+    let ans3 = a
+        .citer()
+        .tuple_windows()
+        .filter(|&(a0, a1)| a0 >= a1)
+        .enumerate()
+        .flat_map(|(i, (a0, a1))| it![(a1, None, i), (a0, Some(a1), i)])
+        .sorted()
+        .scan(
+            (BinaryHeap::<Reverse<(i64, usize)>>::new(), vec![false; n]),
+            |(q, inserted), (x, pair, idx)| {
+                if let Some(pair) = pair {
+                    inserted[idx] = false;
+
+                    while matches!(q.peek(), Some(&Reverse((_, idx2))) if !inserted[idx2]) {
+                        q.pop();
+                    }
+
+                    if let Some(&Reverse((y, _))) = q.peek() {
+                        Some(Some(2 * (x - max(y, pair))))
+                    } else {
+                        Some(None)
+                    }
+                } else {
+                    q.push(Reverse((x, idx)));
+                    inserted[idx] = true;
+                    Some(None)
+                }
+            },
+        )
+        .flatten()
+        .max()
+        .map_or(ans0, |x| ans0 - x);
+
+    let ans4 = a
+        .citer()
+        .tuple_windows()
+        .filter(|&(a0, a1)| a0 <= a1)
+        .enumerate()
+        .flat_map(|(i, (a0, a1))| it![(a0, None, i), (a1, Some(a0), i)])
+        .sorted()
+        .scan(
+            (BinaryHeap::<Reverse<(i64, usize)>>::new(), vec![false; n]),
+            |(q, inserted), (x, pair, idx)| {
+                if let Some(pair) = pair {
+                    inserted[idx] = false;
+
+                    while matches!(q.peek(), Some(&Reverse((_, idx2))) if !inserted[idx2]) {
+                        q.pop();
+                    }
+
+                    if let Some(&Reverse((y, _))) = q.peek() {
+                        Some(Some(2 * (x - max(y, pair))))
+                    } else {
+                        Some(None)
+                    }
+                } else {
+                    q.push(Reverse((x, idx)));
+                    inserted[idx] = true;
+                    Some(None)
+                }
+            },
+        )
+        .flatten()
+        .max()
+        .map_or(ans0, |x| ans0 - x);
+
+    let ans = min(ans0, min(min(ans1, ans2), min(ans3, ans4)));
+    println!("{}", ans);
+}
