955d644791aca7cbb56aac91304d2b39b2819ece
hiratasa <36002166+hiratasa@users.noreply.github.com>
2022-06-26 17:24:19 +0900

    双対セグ木使った実装に変更

--- abc257-rs/src/g.rs
+++ abc257-rs/src/g.rs
@@ -191,223 +191,98 @@ fn z_algorithm<T: std::cmp::Eq>(s: &[T]) -> Vec<usize> {
 
 trait Monoid {
     type Item: Clone;
-
     fn id() -> Self::Item;
     fn op(lhs: &Self::Item, rhs: &Self::Item) -> Self::Item;
 }
-
-// M: Monoid of value
-// Op: Monoid of lazy operation
 #[derive(Debug)]
-struct LazySegmentTree<M, Op>
+struct DualSegmentTree<Op>
 where
-    M: Monoid,
     Op: Monoid,
 {
     height: usize,
     cap: usize,
-    values: Vec<M::Item>,
     lazy: Vec<Op::Item>,
 }
-
-trait Operator<T>: Monoid {
-    fn apply(op: &Self::Item, v: &T) -> T;
-}
-
 #[allow(dead_code)]
-impl<M, Op> LazySegmentTree<M, Op>
+impl<Op> DualSegmentTree<Op>
 where
-    M: Monoid,
-    Op: Monoid + Operator<M::Item>,
+    Op: Monoid,
 {
     fn new(n: usize) -> Self {
         let cap = n.next_power_of_two();
-        LazySegmentTree {
+        DualSegmentTree {
             height: cap.trailing_zeros() as usize + 1,
             cap,
-            values: vec![M::id(); 2 * cap - 1],
             lazy: vec![Op::id(); 2 * cap - 1],
         }
     }
-
-    fn with(vals: &Vec<M::Item>) -> Self {
+    fn with(vals: &Vec<Op::Item>) -> Self {
         let n = vals.len();
         let cap = n.next_power_of_two();
-
-        let mut values = Vec::with_capacity(2 * cap - 1);
-        values.resize(cap - 1, M::id());
-        values.extend(vals.iter().cloned());
-        values.resize(2 * cap - 1, M::id());
-
-        let mut st = LazySegmentTree {
+        let mut lazy = Vec::with_capacity(2 * cap - 1);
+        lazy.resize(cap - 1, Op::id());
+        lazy.extend(vals.iter().cloned());
+        lazy.resize(2 * cap - 1, Op::id());
+        DualSegmentTree {
             height: cap.trailing_zeros() as usize + 1,
             cap,
-            values,
-            lazy: vec![Op::id(); 2 * cap - 1],
-        };
-
-        for idx in (0..cap - 1).rev() {
-            st.fix_value(idx);
-        }
-
-        st
-    }
-
-    // internal
-    fn get_node_value(&mut self, idx: usize) -> M::Item {
-        Op::apply(&self.lazy[idx], &self.values[idx])
-    }
-
-    // internal
-    fn fix_value(&mut self, idx: usize) {
-        let left_idx = 2 * (idx + 1) - 1;
-        let right_idx = 2 * (idx + 1);
-        if left_idx < self.values.len() {
-            self.values[idx] = M::op(
-                &self.get_node_value(left_idx),
-                &self.get_node_value(right_idx),
-            );
+            lazy,
         }
     }
-
-    // internal
-    fn resolve(&mut self, parent_idx: usize) {
+    fn apply(&mut self, idx: usize, p: &Op::Item) {
+        self.lazy[idx] = Op::op(p, &self.lazy[idx]);
+    }
+    fn push(&mut self, parent_idx: usize) {
         let left_idx = 2 * (parent_idx + 1) - 1;
         let right_idx = 2 * (parent_idx + 1);
-
-        if left_idx < self.values.len() {
-            self.lazy[left_idx] = Op::op(&self.lazy[parent_idx], &self.lazy[left_idx]);
-            self.lazy[right_idx] = Op::op(&self.lazy[parent_idx], &self.lazy[right_idx]);
-            self.lazy[parent_idx] = Op::id();
-            self.fix_value(parent_idx);
-        } else {
-            self.values[parent_idx] = Op::apply(&self.lazy[parent_idx], &self.values[parent_idx]);
+        if left_idx < self.lazy.len() {
+            let l = self.lazy[parent_idx].clone();
+            self.apply(left_idx, &l);
+            self.apply(right_idx, &l);
             self.lazy[parent_idx] = Op::id();
         }
     }
-
-    // internal
-    fn resolve_all(&mut self, pos: usize) {
-        let idx = self.cap - 1 + pos;
-        for i in (0..self.height).rev() {
-            self.resolve(((idx + 1) >> i) - 1);
+    fn push_all(&mut self, idx: usize) {
+        for i in (1..(idx + 2).next_power_of_two().trailing_zeros()).rev() {
+            self.push(((idx + 1) >> i) - 1);
         }
     }
-
-    fn get(&mut self, pos: usize) -> M::Item {
-        self.resolve_all(pos);
-
+    fn get(&mut self, pos: usize) -> Op::Item {
         let idx = self.cap - 1 + pos;
-        self.values[idx].clone()
+        self.push_all(idx);
+        self.lazy[idx].clone()
     }
-
-    fn set(&mut self, pos: usize, v: M::Item) {
-        self.resolve_all(pos);
-
-        let mut idx = self.cap - 1 + pos;
-        self.values[idx] = v;
-        self.lazy[idx] = Op::id();
-
-        while idx > 0 {
-            idx = (idx - 1) / 2;
-            self.fix_value(idx);
-        }
+    fn set(&mut self, pos: usize, p: Op::Item) {
+        let idx = self.cap - 1 + pos;
+        self.push_all(idx);
+        self.lazy[idx] = p;
     }
-
     fn update(&mut self, a: usize, b: usize, p: Op::Item) {
         let mut left_idx = a + self.cap - 1;
         let mut right_idx = b + self.cap - 1;
-
-        // Opが非可換の場合用に, これより前にupdateされたものを適用させておく
-        for i in (1..self.height).rev() {
-            self.resolve(((left_idx + 1) >> i) - 1);
-            self.resolve(((right_idx + 1) >> i) - 1);
-        }
-
+        self.push_all(((left_idx + 1) >> (left_idx + 1).trailing_zeros()) - 1);
+        self.push_all(((right_idx + 1) >> (right_idx + 1).trailing_zeros()) - 1);
         while left_idx < right_idx {
             if left_idx % 2 == 0 {
-                self.lazy[left_idx] = Op::op(&p, &self.lazy[left_idx]);
+                self.apply(left_idx, &p);
             }
-
             if right_idx % 2 == 0 {
-                self.lazy[right_idx - 1] = Op::op(&p, &self.lazy[right_idx - 1]);
+                self.apply(right_idx - 1, &p);
             }
-
-            // 偶数の場合は一つ右隣の親になる
             left_idx = left_idx >> 1;
             right_idx = (right_idx - 1) >> 1;
         }
-
-        let mut left_idx = a + self.cap - 1;
-        let mut right_idx = b + self.cap - 1;
-        for _ in 0..self.height - 1 {
-            left_idx = (left_idx - 1) >> 1;
-            self.fix_value(left_idx);
-
-            right_idx = (right_idx - 1) >> 1;
-            // This is out of bounds if b == self.cap.
-            // (currently checked in fix_value())
-            self.fix_value(right_idx);
-        }
-    }
-
-    fn query(&mut self, a: usize, b: usize) -> M::Item {
-        let mut left = M::id();
-        let mut right = M::id();
-
-        let mut left_idx = a + self.cap - 1;
-        let mut right_idx = b + self.cap - 1;
-
-        let c0 = std::cmp::min(
-            // trailing_ones
-            (!left_idx).trailing_zeros(),
-            (right_idx + 1).trailing_zeros(),
-        ) as usize;
-
-        for i in (c0 + 1..self.height).rev() {
-            self.resolve(((left_idx + 1) >> i) - 1);
-            self.resolve(((right_idx + 1) >> i) - 1);
-        }
-
-        left_idx = left_idx >> c0;
-        right_idx = ((right_idx + 1) >> c0) - 1;
-
-        while left_idx < right_idx {
-            if left_idx % 2 == 0 {
-                left = M::op(&left, &self.get_node_value(left_idx));
-                left_idx += 1;
-            }
-
-            if right_idx % 2 == 0 {
-                right = M::op(&self.get_node_value(right_idx - 1), &right);
-                right_idx -= 1;
-            }
-
-            let c = std::cmp::min(
-                // trailing_ones
-                (!left_idx).trailing_zeros(),
-                (right_idx + 1).trailing_zeros(),
-            );
-            left_idx = left_idx >> c;
-            right_idx = ((right_idx + 1) >> c) - 1;
-        }
-
-        M::op(&left, &right)
     }
 }
-
 macro_rules! define_monoid {
-    ($name: ident, $t: ty, $id: expr, $op: expr) => {
+    ($ name : ident , $ t : ty , $ id : expr , $ op : expr ) => {
         #[derive(Clone, Debug)]
         struct $name;
-
         impl Monoid for $name {
             type Item = $t;
-
             fn id() -> Self::Item {
                 $id
             }
-
             fn op(lhs: &Self::Item, rhs: &Self::Item) -> Self::Item {
                 ($op)(*lhs, *rhs)
             }
@@ -416,12 +291,6 @@ macro_rules! define_monoid {
 }
 
 define_monoid!(Minimum, i64, 1 << 60, i64::min);
-
-impl Operator<i64> for Minimum {
-    fn apply(op: &Self::Item, v: &i64) -> i64 {
-        min(*op, *v)
-    }
-}
 
 fn main() {
     let s = read_str();
@@ -433,7 +302,7 @@ fn main() {
     let z = z_algorithm(&chain(s, t).collect::<Vec<_>>());
 
     let mut st = (0..m).fold(
-        LazySegmentTree::<Minimum, Minimum>::with(&vvec![0; 1<<60; m + 1]),
+        DualSegmentTree::<Minimum>::with(&vvec![0; 1<<60; m + 1]),
         |mut st, i| {
             let l = min(n, z[n + i]);
 
