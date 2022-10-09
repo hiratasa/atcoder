f146528cfdafd41eceea2bd73f4811102e5ab7aa
hiratasa <36002166+hiratasa@users.noreply.github.com>
2022-10-10 04:16:37 +0900

    Add codes

--- arc104-rs/src/e.rs
+++ arc104-rs/src/e.rs
@@ -16,7 +16,7 @@ use std::usize;
 #[allow(unused_imports)]
 use bitset_fixed::BitSet;
 #[allow(unused_imports)]
-use itertools::{chain, iproduct, iterate, izip, Itertools};
+use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
 #[allow(unused_imports)]
 use itertools_num::ItertoolsNum;
 #[allow(unused_imports)]
@@ -56,7 +56,9 @@ macro_rules! it {
 macro_rules! bitset {
     ($n:expr, $x:expr) => {{
         let mut bs = BitSet::new($n);
-        bs.buffer_mut()[0] = $x as u64;
+        if $n > 0 {
+            bs.buffer_mut()[0] = $x as u64;
+        }
         bs
     }};
 }
@@ -108,6 +110,14 @@ fn read_str() -> Vec<char> {
 }
 
 #[allow(dead_code)]
+fn read_digits() -> Vec<usize> {
+    read::<String>()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
+}
+
+#[allow(dead_code)]
 fn read_row<T: FromStr>() -> Vec<T> {
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
@@ -131,6 +141,15 @@ fn read_mat<T: FromStr>(n: usize) -> Vec<Vec<T>> {
 #[allow(dead_code)]
 fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
     (0..n).map(|_| f()).collect()
+}
+
+#[allow(dead_code)]
+fn println_opt<T: Copy + std::fmt::Display>(ans: Option<T>) {
+    if let Some(ans) = ans {
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
 }
 
 trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
@@ -235,6 +254,7 @@ impl<M> std::fmt::Debug for Mod<M> {
 impl<T, M: Modulus> std::convert::From<T> for Mod<M>
 where
     usize: std::convert::TryFrom<T>,
+    T: num::traits::Unsigned,
 {
     fn from(v: T) -> Self {
         use std::convert::TryFrom;
@@ -253,73 +273,117 @@ impl<M: Modulus> std::ops::Neg for Mod<M> {
         Mod::new(M::modulus() - self.0)
     }
 }
-impl<T, M: Modulus> std::ops::Add<T> for Mod<M>
-where
-    T: std::convert::Into<Mod<M>>,
-{
+impl<M: Modulus> std::ops::Add<Mod<M>> for Mod<M> {
     type Output = Self;
-    fn add(self, rhs: T) -> Self {
-        Mod::new(self.0 + rhs.into().0)
+    fn add(self, rhs: Mod<M>) -> Self {
+        Mod::new(self.0 + rhs.0)
+    }
+}
+impl<M: Modulus> std::ops::Add<usize> for Mod<M> {
+    type Output = Self;
+    fn add(self, rhs: usize) -> Self {
+        Mod::new(self.0 + rhs)
+    }
+}
+impl<M: Modulus> std::ops::Add<Mod<M>> for usize {
+    type Output = Mod<M>;
+    fn add(self, rhs: Mod<M>) -> Mod<M> {
+        Mod::new(self + rhs.0)
     }
 }
 impl<T, M: Modulus> std::ops::AddAssign<T> for Mod<M>
 where
-    T: std::convert::Into<Mod<M>>,
+    Mod<M>: std::ops::Add<T, Output = Mod<M>>,
 {
     fn add_assign(&mut self, rhs: T) {
         *self = *self + rhs;
     }
 }
-impl<T, M: Modulus> std::ops::Sub<T> for Mod<M>
-where
-    T: std::convert::Into<Mod<M>>,
-{
+impl<M: Modulus> std::ops::Sub<Mod<M>> for Mod<M> {
     type Output = Self;
-    fn sub(self, rhs: T) -> Self {
-        Mod::new(self.0 + M::modulus() - rhs.into().0)
+    fn sub(self, rhs: Mod<M>) -> Self {
+        Mod::new(self.0 + M::modulus() - rhs.0)
+    }
+}
+impl<M: Modulus> std::ops::Sub<usize> for Mod<M> {
+    type Output = Self;
+    fn sub(self, rhs: usize) -> Self {
+        Mod::new(self.0 + M::modulus() - rhs % M::modulus())
+    }
+}
+impl<M: Modulus> std::ops::Sub<Mod<M>> for usize {
+    type Output = Mod<M>;
+    fn sub(self, rhs: Mod<M>) -> Mod<M> {
+        Mod::new(self + M::modulus() - rhs.0)
     }
 }
 impl<T, M: Modulus> std::ops::SubAssign<T> for Mod<M>
 where
-    T: std::convert::Into<Mod<M>>,
+    Mod<M>: std::ops::Sub<T, Output = Mod<M>>,
 {
     fn sub_assign(&mut self, rhs: T) {
         *self = *self - rhs;
     }
 }
-impl<T, M: Modulus> std::ops::Mul<T> for Mod<M>
-where
-    T: std::convert::Into<Mod<M>>,
-{
+impl<M: Modulus> std::ops::Mul<Mod<M>> for Mod<M> {
     type Output = Self;
-    fn mul(self, rhs: T) -> Self {
-        Mod::new(self.0 * rhs.into().0)
+    fn mul(self, rhs: Mod<M>) -> Self {
+        Mod::new(self.0 * rhs.0)
+    }
+}
+impl<M: Modulus> std::ops::Mul<usize> for Mod<M> {
+    type Output = Self;
+    fn mul(self, rhs: usize) -> Self {
+        Mod::new(self.0 * (rhs % M::modulus()))
+    }
+}
+impl<M: Modulus> std::ops::Mul<Mod<M>> for usize {
+    type Output = Mod<M>;
+    fn mul(self, rhs: Mod<M>) -> Mod<M> {
+        Mod::new((self % M::modulus()) * rhs.0)
     }
 }
 impl<T, M: Modulus> std::ops::MulAssign<T> for Mod<M>
 where
-    T: std::convert::Into<Mod<M>>,
+    Mod<M>: std::ops::Mul<T, Output = Mod<M>>,
 {
     fn mul_assign(&mut self, rhs: T) {
         *self = *self * rhs;
     }
 }
-impl<T, M: Modulus> std::ops::Div<T> for Mod<M>
-where
-    T: std::convert::Into<Mod<M>>,
-{
+impl<M: Modulus> std::ops::Div<Mod<M>> for Mod<M> {
     type Output = Self;
-    fn div(self, rhs: T) -> Self {
+    fn div(self, rhs: Mod<M>) -> Self {
         if self.0 == 0 {
             self
         } else {
-            self * rhs.into().inv()
+            self * rhs.inv()
+        }
+    }
+}
+impl<M: Modulus> std::ops::Div<usize> for Mod<M> {
+    type Output = Self;
+    fn div(self, rhs: usize) -> Self {
+        if self.0 == 0 {
+            self
+        } else {
+            self * Mod::new(rhs).inv()
+        }
+    }
+}
+impl<M: Modulus> std::ops::Div<Mod<M>> for usize {
+    type Output = Mod<M>;
+    fn div(self, rhs: Mod<M>) -> Mod<M> {
+        if self == 0 {
+            Mod::new(self)
+        } else {
+            self * rhs.inv()
         }
     }
 }
 impl<T, M: Modulus> std::ops::DivAssign<T> for Mod<M>
 where
-    T: std::convert::Into<Mod<M>>,
+    Mod<M>: std::ops::Div<T, Output = Mod<M>>,
 {
     fn div_assign(&mut self, rhs: T) {
         *self = *self / rhs;
@@ -358,311 +422,93 @@ impl<M: Modulus> num::One for Mod<M> {
     }
 }
 
-const DEG: usize = 6;
-
-#[derive(Debug, Clone, Copy)]
-struct Polynomial {
-    coeff: [Mod1000000007; DEG + 1],
-}
-
-impl Polynomial {
-    fn new(coeff: &[Mod1000000007]) -> Polynomial {
-        assert!(coeff.len() <= DEG + 1);
-
-        let mut c = [Mod::zero(); DEG + 1];
-        for i in 0..coeff.len() {
-            c[i] = coeff[i];
-        }
-
-        Polynomial { coeff: c }
-    }
-
-    fn zero() -> Polynomial {
-        Polynomial::new(&[Mod::zero()])
-    }
-
-    fn one() -> Polynomial {
-        Polynomial::new(&[Mod::one()])
-    }
-}
-
-impl std::ops::Mul<Mod1000000007> for Polynomial {
-    type Output = Polynomial;
-
-    fn mul(self, rhs: Mod1000000007) -> Self::Output {
-        let mut coeff = [Mod::zero(); DEG + 1];
-        for i in 0..=DEG {
-            coeff[i] = self.coeff[i] * rhs;
-        }
-
-        Polynomial::new(&coeff)
-    }
-}
-
-impl std::ops::Mul<Polynomial> for Polynomial {
-    type Output = Polynomial;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        let mut coeff = [Mod::zero(); DEG + 1];
-        for i in 0..=DEG {
-            for j in 0..=DEG {
-                if i + j > DEG {
-                    assert!(self.coeff[i].is_zero() || rhs.coeff[j].is_zero());
-                    continue;
+fn lis(v: &[usize]) -> usize {
+    v.citer()
+        .fold(vec![], |mut t, x| {
+            match t.binary_search(&x) {
+                Ok(_) => {}
+                Err(idx) => {
+                    if idx == t.len() {
+                        t.push(x);
+                    } else {
+                        t[idx] = x;
+                    }
                 }
-                coeff[i + j] = coeff[i + j] + self.coeff[i] * rhs.coeff[j];
             }
-        }
-
-        Polynomial::new(&coeff)
-    }
+            t
+        })
+        .len()
 }
 
-impl std::ops::Add<Polynomial> for Polynomial {
-    type Output = Polynomial;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        let mut coeff = [Mod::zero(); DEG + 1];
-        for i in 0..=DEG {
-            coeff[i] = self.coeff[i] + rhs.coeff[i];
-        }
-
-        Polynomial::new(&coeff)
-    }
-}
-
-impl std::ops::Div<usize> for Polynomial {
-    type Output = Polynomial;
-
-    fn div(self, rhs: usize) -> Self::Output {
-        let mut coeff = self.coeff.clone();
-
-        for i in 0..=DEG {
-            coeff[i] /= rhs;
-        }
-
-        Polynomial::new(&coeff)
-    }
-}
-
-// sum[t=x to upper] t^deg
-fn sum0_impl(deg: usize, upper: usize) -> Polynomial {
-    let u = Mod::new(upper);
-    match deg {
-        0 => {
-            // u + 1 - x
-            Polynomial::new(&[u + 1, -Mod::one()])
-        }
-        1 => {
-            // (u+x)*(u+1-x)/2
-            Polynomial::new(&[u, Mod::one()]) * Polynomial::new(&[u + 1, -Mod::one()]) / 2
-        }
-        2 => {
-            // (2u^2+u+2ux-x+2x^2)*(u+1-x)/6
-            Polynomial::new(&[u.pow(2) * 2 + u, u * 2 - 1, Mod::new(2)])
-                * Polynomial::new(&[u + 1, -Mod::one()])
-                / 6
-        }
-        3 => {
-            // (u^2+u-x+x^2)*(u+x)*(u+1-x)/4
-            Polynomial::new(&[u.pow(2) + u, -Mod::one(), Mod::one()])
-                * Polynomial::new(&[u, Mod::one()])
-                * Polynomial::new(&[u + 1, -Mod::one()])
-                / 4
-        }
-        4 => {
-            // (u^5/5+u^4/2+u^3/3-u/30 + x/30 - x^3/3 + x^4/2 - x^5/5)
-            Polynomial::new(&[
-                u.pow(5) / 5 + u.pow(4) / 2 + u.pow(3) / 3 - u / 30,
-                Mod::one() / 30,
-                Mod::zero(),
-                -Mod::one() / 3,
-                Mod::one() / 2,
-                -Mod::one() / 5,
-            ])
-        }
-        5 => {
-            // (2u^6+6u^5+5u^4-u^2 + x^2 - 5x^4 + 6x^5 - 2x^6) / 12
-            Polynomial::new(&[
-                u.pow(6) * 2 + u.pow(5) * 6 + u.pow(4) * 5 - u.pow(2),
-                Mod::zero(),
-                Mod::one(),
-                Mod::zero(),
-                -Mod::new(5),
-                Mod::new(6),
-                -Mod::new(2),
-            ]) / 12
-        }
-        _ => unreachable!(),
-    }
-}
-
-// sum[t=x+1 to upper] t^deg
-fn sum1_impl(deg: usize, upper: usize) -> Polynomial {
-    let u = Mod::new(upper);
-    match deg {
-        0 => {
-            // u - x
-            Polynomial::new(&[u, -Mod::one()])
-        }
-        1 => {
-            // (u+1+x)*(u-x)/2
-            Polynomial::new(&[u + 1, Mod::one()]) * Polynomial::new(&[u, -Mod::one()]) / 2
-        }
-        2 => {
-            // (2u^2+3u+1+2ux+3x+2x^2)*(u-x)/6
-            Polynomial::new(&[u.pow(2) * 2 + u * 3 + 1, u * 2 + 3, Mod::new(2)])
-                * Polynomial::new(&[u, -Mod::one()])
-                / 6
-        }
-        3 => {
-            // (u^2+u+x+x^2)*(u+1+x)*(u-x)/4
-            Polynomial::new(&[u.pow(2) + u, Mod::one(), Mod::one()])
-                * Polynomial::new(&[u + 1, Mod::one()])
-                * Polynomial::new(&[u, -Mod::one()])
-                / 4
-        }
-        4 => {
-            // (u^5/5+u^4/2+u^3/3-u/30 + x/30 - x^3/3 - x^4/2 - x^5/5)
-            Polynomial::new(&[
-                u.pow(5) / 5 + u.pow(4) / 2 + u.pow(3) / 3 - u / 30,
-                Mod::one() / 30,
-                Mod::zero(),
-                -Mod::one() / 3,
-                -Mod::one() / 2,
-                -Mod::one() / 5,
-            ])
-        }
-        5 => {
-            // (2u^6+6u^5+5u^4-u^2 + x^2 - 5x^4 - 6x^5 - 2x^6) / 12
-            Polynomial::new(&[
-                u.pow(6) * 2 + u.pow(5) * 6 + u.pow(4) * 5 - u.pow(2),
-                Mod::zero(),
-                Mod::one(),
-                Mod::zero(),
-                -Mod::new(5),
-                -Mod::new(6),
-                -Mod::new(2),
-            ]) / 12
-        }
-        _ => unreachable!(),
-    }
-}
-
-// sum[t=x to upper] poly
-fn sum0(poly: &Polynomial, upper: usize) -> Polynomial {
-    assert!(poly.coeff[DEG].is_zero());
-    (0..DEG)
-        .map(|i| sum0_impl(i, upper) * poly.coeff[i])
-        .fold(Polynomial::new(&[]), |ret, q| ret + q)
-}
-
-// sum[t=x+1 to upper] poly
-fn sum1(poly: &Polynomial, upper: usize) -> Polynomial {
-    assert!(poly.coeff[DEG].is_zero());
-    (0..DEG)
-        .map(|i| sum1_impl(i, upper) * poly.coeff[i])
-        .fold(Polynomial::new(&[]), |ret, q| ret + q)
-}
-
-fn len_lis(t: &[(usize, bool)]) -> usize {
-    let n = t.len();
-
-    let mut u = vec![1; n];
-    for i in 1..n {
-        if t[i].1 {
-            u[t[i].0] = u[t[i - 1].0];
-        } else {
-            u[t[i].0] = u[t[i - 1].0] + 1;
-        }
-    }
-
-    let mut dp = vvec![0; usize::MAX; n + 1];
-    for i in 0..n {
-        let j = (0..n).find(|&j| dp[j] < u[i] && u[i] <= dp[j + 1]).unwrap();
-        dp[j + 1] = u[i];
-    }
-
-    dp.citer().take_while(|&x| x != usize::MAX).count() - 1
-}
-
-fn solve(a: &[usize], t: &mut Vec<(usize, bool)>, used: &mut [bool]) -> Mod1000000007 {
+fn calc(
+    a: &[usize],
+    idxs: &FxHashMap<usize, usize>,
+    w: &[usize],
+    nums: &mut Vec<Vec<Vec<usize>>>,
+    i: usize,
+) -> Mod1000000007 {
     let n = a.len();
 
-    if t.len() == n {
-        let l = len_lis(&t);
+    if i == n {
+        let mut v = vec![0; n];
+        nums.iter().flatten().enumerate().for_each(|(j, arr)| {
+            for &idx in arr {
+                v[idx] = j + 1;
+            }
+        });
 
-        let (poly, upper) =
-            t.citer()
-                .rev()
-                .fold((Polynomial::one(), usize::MAX), |(poly, upper), (i, eq)| {
-                    if eq {
-                        (poly, min(upper, a[i] + 1))
-                    } else {
-                        (
-                            sum1(&poly, min(upper.saturating_sub(1), a[i])),
-                            min(upper.saturating_sub(1), a[i]),
-                        )
-                    }
-                });
-
-        return if upper == 0 {
-            Mod::zero()
-        } else {
-            poly.coeff[0] * l
-        };
+        return Mod::new(lis(&v));
     }
 
-    // eq
-    let z0 = if !t.is_empty() {
-        let last = t[t.len() - 1].0;
-        (0..n)
-            .map(|i| {
-                if (a[last], last) < (a[i], i) && !used[i] {
-                    used[i] = true;
-                    t.push((i, true));
-                    let x = solve(&a, t, used);
-                    t.pop();
-                    used[i] = false;
+    let mut ex = Mod::zero();
+    for j in 0..idxs[&a[i]] {
+        let p = Mod::new(w[j]) / a[i];
 
-                    x
-                } else {
-                    Mod::zero()
-                }
-            })
-            .sum::<Mod<_>>()
-    } else {
-        Mod::zero()
-    };
+        let m = nums[j].len();
 
-    // greater
-    let z1 = (0..n)
-        .map(|i| {
-            if !used[i] {
-                used[i] = true;
-                t.push((i, false));
-                let x = solve(&a, t, used);
-                t.pop();
-                used[i] = false;
-
-                x
-            } else {
-                Mod::zero()
+        for k in 0..=m {
+            // k個目の前
+            {
+                let q = Mod::new(w[j].saturating_sub(m)) / w[j] / (m + 1);
+                nums[j].insert(k, vec![i]);
+                ex += calc(a, idxs, w, nums, i + 1) * p * q;
+                nums[j].remove(k);
             }
-        })
-        .sum::<Mod<_>>();
 
-    z0 + z1
+            // k個目と同じ
+            if k != m {
+                let q = 1 / Mod::new(w[j]);
+                nums[j][k].push(i);
+                ex += calc(a, idxs, w, nums, i + 1) * p * q;
+                nums[j][k].pop();
+            }
+        }
+    }
+
+    ex
 }
 
 fn main() {
     let n = read::<usize>();
-
     let a = read_row::<usize>();
 
-    let t = a.citer().map(|aa| Mod::new(aa)).product::<Mod1000000007>();
+    let vals = once(0)
+        .chain(a.citer())
+        .sorted()
+        .dedup()
+        .collect::<Vec<_>>();
+    let idxs = vals
+        .citer()
+        .enumerate()
+        .map(|(i, x)| (x, i))
+        .collect::<FxHashMap<_, _>>();
+    let w = vals
+        .citer()
+        .tuple_windows()
+        .map(|(x0, x1)| x1 - x0)
+        .collect::<Vec<_>>();
 
-    let ans = solve(&a, &mut vec![], &mut vec![false; n]) / t;
+    let ans = calc(&a, &idxs, &w, &mut vec![vec![]; vals.len()], 0);
 
     println!("{}", ans);
 }
