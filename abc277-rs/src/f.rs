a2c3aed5e922016a434bf39bdceee721b52f2c84
hiratasa <36002166+hiratasa@users.noreply.github.com>
2022-11-13 01:45:37 +0900

    Add codes

---
+++ abc277-rs/src/f.rs
@@ -0,0 +1,254 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::f64;
+#[allow(unused_imports)]
+use std::i64;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rand::{rngs::SmallRng, seq::IteratorRandom, seq::SliceRandom, Rng, SeedableRng};
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+
+// vec with some initial value
+#[allow(unused_macros)]
+macro_rules! vvec {
+    ($($x:expr),+; $y:expr; $n:expr) => {{
+        let mut v = vec![$y; $n];
+
+        let mut it = v.iter_mut();
+        $(
+            *it.next().unwrap() = $x;
+        )+
+
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! it {
+    ($x:expr) => {
+        once($x)
+    };
+    ($first:expr,$($x:expr),+) => {
+        chain(
+            once($first),
+            it!($($x),+)
+        )
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        if $n > 0 {
+            bs.buffer_mut()[0] = $x as u64;
+        }
+        bs
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! pushed {
+    ($c:expr, $x:expr) => {{
+        let x = $x;
+        let mut c = $c;
+        c.push(x);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! inserted {
+    ($c:expr, $($x:expr),*) => {{
+        let mut c = $c;
+        c.insert($($x),*);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! read_tuple {
+    ($($t:ty),+) => {{
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let mut it = line.trim()
+            .split_whitespace();
+
+        ($(
+            it.next().unwrap().parse::<$t>().ok().unwrap()
+        ),+)
+    }}
+}
+
+#[allow(dead_code)]
+fn read<T: FromStr>() -> T {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    line.trim().to_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_str() -> Vec<char> {
+    read::<String>().chars().collect()
+}
+
+#[allow(dead_code)]
+fn read_digits() -> Vec<usize> {
+    read::<String>()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_row<T: FromStr>() -> Vec<T> {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    line.trim()
+        .split_whitespace()
+        .map(|s| s.parse().ok().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_col<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read()).collect()
+}
+
+#[allow(dead_code)]
+fn read_mat<T: FromStr>(n: usize) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_row()).collect()
+}
+
+#[allow(dead_code)]
+fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
+    (0..n).map(|_| f()).collect()
+}
+
+#[allow(dead_code)]
+fn println_opt<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(ans) = ans {
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
+}
+
+trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
+where
+    T: 'a + Copy,
+{
+    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
+        self.into_iter().copied()
+    }
+}
+
+impl<'a, T, I> IterCopyExt<'a, T> for I
+where
+    I: IntoIterator<Item = &'a T>,
+    T: 'a + Copy,
+{
+}
+
+fn dfs(
+    edges: &[Vec<usize>],
+    v: usize,
+    p: usize,
+    visiting: &mut [bool],
+    visited: &mut [bool],
+) -> bool {
+    if visited[v] {
+        true
+    } else if visiting[v] {
+        false
+    } else {
+        visiting[v] = true;
+
+        let ok = edges[v]
+            .citer()
+            .filter(|&u| u != p)
+            .map(|u| dfs(edges, u, v, visiting, visited))
+            .all(|r| r);
+
+        visiting[v] = false;
+        visited[v] = true;
+
+        ok
+    }
+}
+
+fn main() {
+    let (h, w) = read_tuple!(usize, usize);
+    let a = read_mat::<usize>(h);
+
+    let adjs = a
+        .iter()
+        .enumerate()
+        .fold(vec![vec![]; (h + 1) * w], |adjs, (i, row)| {
+            row.citer()
+                .enumerate()
+                .filter(|&(_, x)| x != 0)
+                .map(|(j, x)| (x, j))
+                .sorted()
+                .group_by(|&(x, _)| x)
+                .into_iter()
+                .enumerate()
+                .fold(adjs, |adjs, (idx, (_x, it))| {
+                    it.fold(adjs, |mut adjs, (_, j)| {
+                        adjs[i * w + idx].push(h * w + j);
+                        if idx + 1 < w {
+                            adjs[h * w + j].push(i * w + (idx + 1));
+                        }
+                        adjs
+                    })
+                })
+        });
+
+    if (0..adjs.len())
+        .scan(
+            (vec![false; adjs.len()], vec![false; adjs.len()]),
+            |(visiting, visited), v| Some(dfs(&adjs, v, adjs.len(), visiting, visited)),
+        )
+        .any(|ok| !ok)
+    {
+        println!("No");
+        return;
+    }
+
+    if !a
+        .iter()
+        .filter_map(|row| row.citer().filter(|&x| x != 0).minmax().into_option())
+        .sorted()
+        .tuple_windows()
+        .all(|((_x0, x1), (x2, _x3))| x1 <= x2)
+    {
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
+}
