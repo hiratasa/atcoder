66e156f66ad17f55e9d36aa4932cadfc35283afc
hiratasa <36002166+hiratasa@users.noreply.github.com>
2022-11-25 02:58:10 +0900

    Add codes

--- abc129-rs/src/f.rs
+++ abc129-rs/src/f.rs
@@ -3,6 +3,10 @@ use std::cmp::*;
 #[allow(unused_imports)]
 use std::collections::*;
 #[allow(unused_imports)]
+use std::f64;
+#[allow(unused_imports)]
+use std::i64;
+#[allow(unused_imports)]
 use std::io;
 #[allow(unused_imports)]
 use std::iter::*;
@@ -14,9 +18,13 @@ use std::str::*;
 use std::usize;
 
 #[allow(unused_imports)]
-use itertools::{chain, iproduct, iterate, izip, Itertools};
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
 #[allow(unused_imports)]
 use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rand::{rngs::SmallRng, seq::IteratorRandom, seq::SliceRandom, Rng, SeedableRng};
 #[allow(unused_imports)]
 use rustc_hash::FxHashMap;
 #[allow(unused_imports)]
@@ -47,14 +55,29 @@ macro_rules! it {
             once($first),
             it!($($x),+)
         )
-    }
+    };
+    ($($x:expr),+,) => {
+        it![$($x),+]
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        if $n > 0 {
+            bs.buffer_mut()[0] = $x as u64;
+        }
+        bs
+    }};
 }
 
 #[allow(unused_macros)]
 macro_rules! pushed {
     ($c:expr, $x:expr) => {{
+        let x = $x;
         let mut c = $c;
-        c.push($x);
+        c.push(x);
         c
     }};
 }
@@ -96,6 +119,14 @@ fn read_str() -> Vec<char> {
 }
 
 #[allow(dead_code)]
+fn read_digits() -> Vec<usize> {
+    read::<String>()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
+}
+
+#[allow(dead_code)]
 fn read_row<T: FromStr>() -> Vec<T> {
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
@@ -121,6 +152,15 @@ fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
     (0..n).map(|_| f()).collect()
 }
 
+#[allow(dead_code)]
+fn println_opt<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(ans) = ans {
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
+}
+
 trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
 where
     T: 'a + Copy,
@@ -137,4 +177,144 @@ where
 {
 }
 
-fn main() {}
+#[allow(dead_code)]
+pub fn pow_mod(mut x: usize, mut p: usize, m: usize) -> usize {
+    let mut y = 1;
+    x = x % m;
+    while p > 0 {
+        if p & 1 > 0 {
+            y = y * x % m;
+        }
+        x = x * x % m;
+        p >>= 1;
+    }
+    y
+}
+
+#[allow(dead_code)]
+fn extgcd(a: i64, b: i64) -> (i64, i64, i64) {
+    let (_zero, g, _u, v) = std::iter::successors(Some((a, b, 1, 0)), |&(a, b, u, v)| {
+        if a == 0 {
+            None
+        } else {
+            Some((b % a, a, -u * (b / a) + v, u))
+        }
+    })
+    .last()
+    .unwrap();
+
+    (v, (g - a * v) / b, g)
+}
+
+#[allow(dead_code)]
+fn lower_bound<T, F>(mut begin: T, mut end: T, epsilon: T, f: F) -> T
+where
+    T: std::marker::Copy
+        + std::ops::Add<T, Output = T>
+        + std::ops::Sub<T, Output = T>
+        + std::ops::Div<T, Output = T>
+        + std::cmp::PartialOrd<T>
+        + std::convert::TryFrom<i32>,
+    F: Fn(T) -> std::cmp::Ordering,
+{
+    let two = T::try_from(2).ok().unwrap();
+    while end - begin >= epsilon {
+        let mid = begin + (end - begin) / two;
+        match f(mid) {
+            std::cmp::Ordering::Less => {
+                begin = mid + epsilon;
+            }
+            _ => {
+                end = mid;
+            }
+        }
+    }
+    begin
+}
+
+#[allow(dead_code)]
+fn lower_bound_int<T, F>(begin: T, end: T, f: F) -> T
+where
+    T: std::marker::Copy
+        + std::ops::Add<T, Output = T>
+        + std::ops::Sub<T, Output = T>
+        + std::ops::Div<T, Output = T>
+        + std::cmp::PartialOrd<T>
+        + std::convert::TryFrom<i32>,
+    F: Fn(T) -> std::cmp::Ordering,
+{
+    lower_bound(begin, end, T::try_from(1).ok().unwrap(), f)
+}
+
+// sum[i=0 to l-1] (a + b*i) * 10^(i*d)
+fn calc(
+    l: usize,
+    a: usize,
+    b: usize,
+    m: usize,
+    d: usize,
+    memo: &mut FxHashMap<(usize, usize, usize), usize>,
+) -> usize {
+    if l == 0 {
+        return 0;
+    }
+
+    if let Some(&x) = memo.get(&(l, a, b)) {
+        return x;
+    }
+
+    if l % 2 > 0 {
+        return (a % m + calc(l - 1, a + b, b, m, d, memo) * pow_mod(10, d, m) % m) % m;
+    }
+
+    let x = calc(l / 2, 1, 0, m, d, memo);
+    let y = calc(l / 2, 0, 1, m, d, memo);
+
+    let a = a % m;
+    let b = b % m;
+    let l2 = (l / 2) % m;
+    let f2 = pow_mod(10, l / 2 * d, m);
+
+    let r = (a * x % m
+        + a * f2 % m * x % m
+        + b * y % m
+        + b * l2 % m * f2 % m * x % m
+        + b * f2 % m * y % m)
+        % m;
+
+    memo.insert((l, a, b), r);
+
+    r
+}
+
+fn main() {
+    let (l, a, b, m) = read_tuple!(usize, usize, usize, usize);
+
+    let ans = (0..18)
+        .map(|i| lower_bound_int(0, l, |j| (a + b * j).cmp(&(10usize.pow(i)))))
+        .enumerate()
+        .rev()
+        .scan((0, l), |(k, prev), (i, s)| {
+            *k += (*prev - s) * (i + 1);
+            let e = *prev;
+            *prev = s;
+            Some((i, s, e, *k))
+        })
+        .map(|(i, s, e, k)| {
+            let d = i + 1;
+            // sum[i=0 to e-s-1] (a + b * (i+s)) * 10^(k-(i+1)*d)
+            // =sum[i=0 to e-s-1] (a + b * (e-1) - b * i) * 10^(k-(e-s)*d+i*d)
+            calc(
+                e - s,
+                a % m + b % m * ((e + m - 1) % m) % m,
+                (m - b % m) % m,
+                m,
+                d,
+                &mut FxHashMap::default(),
+            ) * pow_mod(10, k - (e - s) * d, m)
+                % m
+        })
+        .fold(0, |x, y| (x + y) % m);
+
+    println!("{}", ans);
+}
