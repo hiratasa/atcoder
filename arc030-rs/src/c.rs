0ad541d22f3488629005e6143b2690954c663420
hiratasa <36002166+hiratasa@users.noreply.github.com>
2023-01-15 20:42:36 +0900

    Add codes

--- arc030-rs/src/c.rs
+++ arc030-rs/src/c.rs
@@ -3,6 +3,10 @@ use std::cmp::*;
 #[allow(unused_imports)]
 use std::collections::*;
 #[allow(unused_imports)]
+use std::f64;
+#[allow(unused_imports)]
+use std::i64;
+#[allow(unused_imports)]
 use std::io;
 #[allow(unused_imports)]
 use std::iter::*;
@@ -14,9 +18,13 @@ use std::str::*;
 use std::usize;
 
 #[allow(unused_imports)]
-use itertools::{chain, iproduct, iterate, izip, Itertools};
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
 #[allow(unused_imports)]
 use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rand::{rngs::SmallRng, seq::IteratorRandom, seq::SliceRandom, Rng, SeedableRng};
 #[allow(unused_imports)]
 use rustc_hash::FxHashMap;
 #[allow(unused_imports)]
@@ -47,14 +55,29 @@ macro_rules! it {
             once($first),
             it!($($x),+)
         )
-    }
+    };
+    ($($x:expr),+,) => {
+        it![$($x),+]
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        if $n > 0 {
+            bs.buffer_mut()[0] = $x as u64;
+        }
+        bs
+    }};
 }
 
 #[allow(unused_macros)]
 macro_rules! pushed {
     ($c:expr, $x:expr) => {{
+        let x = $x;
         let mut c = $c;
-        c.push($x);
+        c.push(x);
         c
     }};
 }
@@ -96,6 +119,14 @@ fn read_str() -> Vec<char> {
 }
 
 #[allow(dead_code)]
+fn read_digits() -> Vec<usize> {
+    read::<String>()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
+}
+
+#[allow(dead_code)]
 fn read_row<T: FromStr>() -> Vec<T> {
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
@@ -121,6 +152,15 @@ fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
     (0..n).map(|_| f()).collect()
 }
 
+#[allow(dead_code)]
+fn println_opt<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(ans) = ans {
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
+}
+
 trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
 where
     T: 'a + Copy,
@@ -137,4 +177,298 @@ where
 {
 }
 
-fn main() {}
+mod detail {
+    #[allow(dead_code)]
+    #[derive(Clone, Copy, Debug)]
+    pub struct Edge<W = ()>
+    where
+        W: Copy,
+    {
+        pub from: usize,
+        pub to: usize,
+        pub label: W,
+    }
+    #[allow(dead_code)]
+    impl<W> Edge<W>
+    where
+        W: Copy,
+    {
+        pub fn new(from: usize, to: usize) -> Self
+        where
+            W: Default,
+        {
+            Self {
+                from,
+                to,
+                label: W::default(),
+            }
+        }
+        pub fn new_with_label(from: usize, to: usize, label: W) -> Self {
+            Self { from, to, label }
+        }
+        pub fn rev(&self) -> Self {
+            Self {
+                from: self.to,
+                to: self.from,
+                ..*self
+            }
+        }
+        pub fn offset1(&self) -> Self {
+            Self {
+                from: self.from - 1,
+                to: self.to - 1,
+                ..*self
+            }
+        }
+    }
+    pub type UnweightedEdge = Edge<()>;
+    pub type WeightedEdge = Edge<usize>;
+    impl std::convert::From<(usize, usize)> for UnweightedEdge {
+        fn from(t: (usize, usize)) -> Self {
+            UnweightedEdge::new(t.0, t.1)
+        }
+    }
+    impl std::convert::From<&(usize, usize)> for UnweightedEdge {
+        fn from(t: &(usize, usize)) -> Self {
+            Edge::from(*t)
+        }
+    }
+    impl std::convert::From<(usize, usize, usize)> for WeightedEdge {
+        fn from(t: (usize, usize, usize)) -> Self {
+            Edge::new_with_label(t.0, t.1, t.2)
+        }
+    }
+    impl std::convert::From<&(usize, usize, usize)> for WeightedEdge {
+        fn from(t: &(usize, usize, usize)) -> Self {
+            Edge::from(*t)
+        }
+    }
+    #[allow(dead_code)]
+    #[derive(Clone, Debug)]
+    pub struct Graph<W = ()>
+    where
+        W: Copy,
+    {
+        pub out_edges: Vec<Vec<Edge<W>>>,
+        pub in_edges: Vec<Vec<Edge<W>>>,
+    }
+    #[allow(dead_code)]
+    pub type UnweightedGraph = Graph<()>;
+    #[allow(dead_code)]
+    pub type WeightedGraph = Graph<usize>;
+    #[allow(dead_code)]
+    impl<W: Copy> Graph<W> {
+        pub fn new(n: usize) -> Self {
+            Self {
+                out_edges: vec![vec![]; n],
+                in_edges: vec![vec![]; n],
+            }
+        }
+        pub fn from_edges_directed<T, I>(n: usize, edges: I) -> Self
+        where
+            I: IntoIterator<Item = T>,
+            T: std::convert::Into<Edge<W>>,
+        {
+            let mut g = Graph::new(n);
+            for edge in edges {
+                let e = edge.into();
+                g.add_edge(e);
+            }
+            g
+        }
+        pub fn from_edges1_directed<T, I>(n: usize, edges: I) -> Self
+        where
+            I: IntoIterator<Item = T>,
+            T: std::convert::Into<Edge<W>>,
+        {
+            Graph::from_edges_directed(n, edges.into_iter().map(|e| e.into()).map(|e| e.offset1()))
+        }
+        pub fn from_edges_undirected<T, I>(n: usize, edges: I) -> Self
+        where
+            I: IntoIterator<Item = T>,
+            T: std::convert::Into<Edge<W>>,
+        {
+            Graph::from_edges_directed(
+                n,
+                edges
+                    .into_iter()
+                    .map(|e| e.into())
+                    .flat_map(|e| std::iter::once(e).chain(std::iter::once(e.rev()))),
+            )
+        }
+        pub fn from_edges1_undirected<T, I>(n: usize, edges: I) -> Self
+        where
+            I: IntoIterator<Item = T>,
+            T: std::convert::Into<Edge<W>>,
+        {
+            Graph::from_edges1_directed(
+                n,
+                edges
+                    .into_iter()
+                    .map(|e| e.into())
+                    .flat_map(|e| std::iter::once(e).chain(std::iter::once(e.rev()))),
+            )
+        }
+        pub fn size(&self) -> usize {
+            self.out_edges.len()
+        }
+        pub fn add_edge<T>(&mut self, e: T)
+        where
+            Edge<W>: std::convert::From<T>,
+        {
+            let edge = Edge::from(e);
+            self.out_edges[edge.from].push(edge);
+            self.in_edges[edge.to].push(edge);
+        }
+        pub fn adjs<'a>(&'a self, v: usize) -> impl 'a + DoubleEndedIterator<Item = usize> {
+            self.out_edges[v].iter().map(|e| e.to)
+        }
+        pub fn children<'a>(
+            &'a self,
+            v: usize,
+            p: usize,
+        ) -> impl 'a + DoubleEndedIterator<Item = usize> {
+            self.adjs(v).filter(move |&u| u != p)
+        }
+        pub fn children_edge<'a>(
+            &'a self,
+            v: usize,
+            p: usize,
+        ) -> impl 'a + DoubleEndedIterator<Item = Edge<W>> {
+            self.out_edges[v].iter().copied().filter(move |e| e.to != p)
+        }
+    }
+}
+
+type Graph = detail::Graph;
+
+#[allow(dead_code)]
+fn dfs(g: &Graph, v: usize, visited: &mut Vec<bool>, vs: &mut Vec<usize>) {
+    visited[v] = true;
+
+    for edge in &g.out_edges[v] {
+        if !visited[edge.to] {
+            dfs(g, edge.to, visited, vs);
+        }
+    }
+
+    vs.push(v);
+}
+
+#[allow(dead_code)]
+fn rev_dfs(
+    g: &Graph,
+    v: usize,
+    idx: usize,
+    idxs: &mut Vec<usize>,
+    vs: &mut Vec<usize>,
+    adjs: &mut Vec<usize>,
+) {
+    idxs[v] = idx;
+    vs.push(v);
+
+    for edge in &g.in_edges[v] {
+        if idxs[edge.from] == std::usize::MAX {
+            rev_dfs(g, edge.from, idx, idxs, vs, adjs);
+        } else if idxs[edge.from] < idx {
+            adjs.push(idxs[edge.from]);
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn scc(g: &Graph) -> (Vec<Vec<usize>>, Vec<usize>, Vec<Vec<usize>>) {
+    let mut vs = vec![];
+    {
+        let mut visited = vec![false; g.size()];
+        for v in 0..g.size() {
+            if !visited[v] {
+                dfs(g, v, &mut visited, &mut vs);
+            }
+        }
+    }
+
+    let mut ret = vec![];
+    let mut idxs = vec![std::usize::MAX; g.size()];
+    let mut scc_edges = vec![];
+    {
+        for &v in vs.iter().rev() {
+            if idxs[v] == std::usize::MAX {
+                let mut component = vec![];
+                let mut adjs = vec![];
+                rev_dfs(g, v, ret.len(), &mut idxs, &mut component, &mut adjs);
+                ret.push(component);
+                scc_edges.push(vec![]);
+                for idx in adjs.iter().copied().sorted().dedup() {
+                    scc_edges[idx].push(ret.len() - 1);
+                }
+            }
+        }
+    }
+
+    (ret, idxs, scc_edges)
+}
+fn main() {
+    let (n, m, k) = read_tuple!(usize, usize, usize);
+    let c = read_row::<char>();
+    let ab = read_vec(m, || read_tuple!(usize, usize));
+
+    let g = Graph::from_edges1_directed(n, ab);
+
+    let (components, _, edges) = scc(&g);
+
+    let freqs = components
+        .iter()
+        .map(|idxs| {
+            idxs.citer().fold(vec![0; 26], |mut freqs, i| {
+                freqs[c[i] as usize - 'a' as usize] += 1;
+                freqs
+            })
+        })
+        .collect::<Vec<_>>();
+
+    let l = components.len();
+
+    let dp = (0..l).rev().fold(
+        vec![vec![None; k + 1]; l],
+        |mut dp: Vec<Vec<Option<Vec<usize>>>>, v| {
+            let empty = vec![];
+            for i in 1..=k {
+                if let Some(s) = freqs[v]
+                    .citer()
+                    .enumerate()
+                    .flat_map(|(c, t)| repeat_n(c, t))
+                    .chain(once(26))
+                    .take(i + 1)
+                    .enumerate()
+                    .find_map(|(j, c)| {
+                        edges[v]
+                            .citer()
+                            .filter_map(|u| dp[u][i - j].as_ref())
+                            .filter(|&s| s[0] < c)
+                            .chain(Some(&empty).filter(|_| i == j))
+                            .min()
+                    })
+                {
+                    dp[v][i] = Some(
+                        freqs[v]
+                            .citer()
+                            .enumerate()
+                            .flat_map(|(c, t)| repeat_n(c, t))
+                            .take(i - s.len())
+                            .chain(s.citer())
+                            .collect::<Vec<_>>(),
+                    );
+                }
+            }
+            dp
+        },
+    );
+
+    println_opt(
+        dp.iter()
+            .filter_map(|t| t[k].as_ref())
+            .min()
+            .map(|s| s.citer().map(|c| ('a' as usize + c) as u8 as char).join("")),
+    );
+}
