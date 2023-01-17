4e78c072e240dd36460c8af236a158f83b04cea6
hiratasa <36002166+hiratasa@users.noreply.github.com>
2023-01-18 01:14:37 +0900

    Add codes

---
+++ abc285-rs/src/f.rs
@@ -0,0 +1,479 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::f64;
+#[allow(unused_imports)]
+use std::i64;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rand::{rngs::SmallRng, seq::IteratorRandom, seq::SliceRandom, Rng, SeedableRng};
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+
+// vec with some initial value
+#[allow(unused_macros)]
+macro_rules! vvec {
+    ($($x:expr),+; $y:expr; $n:expr) => {{
+        let mut v = vec![$y; $n];
+
+        let mut it = v.iter_mut();
+        $(
+            *it.next().unwrap() = $x;
+        )+
+
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! it {
+    ($x:expr) => {
+        once($x)
+    };
+    ($first:expr,$($x:expr),+) => {
+        chain(
+            once($first),
+            it!($($x),+)
+        )
+    };
+    ($($x:expr),+,) => {
+        it![$($x),+]
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        if $n > 0 {
+            bs.buffer_mut()[0] = $x as u64;
+        }
+        bs
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! pushed {
+    ($c:expr, $x:expr) => {{
+        let x = $x;
+        let mut c = $c;
+        c.push(x);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! inserted {
+    ($c:expr, $($x:expr),*) => {{
+        let mut c = $c;
+        c.insert($($x),*);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! read_tuple {
+    ($($t:ty),+) => {{
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let mut it = line.trim()
+            .split_whitespace();
+
+        ($(
+            it.next().unwrap().parse::<$t>().ok().unwrap()
+        ),+)
+    }}
+}
+
+#[allow(dead_code)]
+fn read<T: FromStr>() -> T {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    line.trim().to_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_str() -> Vec<char> {
+    read::<String>().chars().collect()
+}
+
+#[allow(dead_code)]
+fn read_digits() -> Vec<usize> {
+    read::<String>()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_row<T: FromStr>() -> Vec<T> {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    line.trim()
+        .split_whitespace()
+        .map(|s| s.parse().ok().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_col<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read()).collect()
+}
+
+#[allow(dead_code)]
+fn read_mat<T: FromStr>(n: usize) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_row()).collect()
+}
+
+#[allow(dead_code)]
+fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
+    (0..n).map(|_| f()).collect()
+}
+
+#[allow(dead_code)]
+fn println_opt<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(ans) = ans {
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
+}
+
+trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
+where
+    T: 'a + Copy,
+{
+    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
+        self.into_iter().copied()
+    }
+}
+
+impl<'a, T, I> IterCopyExt<'a, T> for I
+where
+    I: IntoIterator<Item = &'a T>,
+    T: 'a + Copy,
+{
+}
+
+fn range(r: impl std::ops::RangeBounds<usize>, n: usize) -> (usize, usize) {
+    let start = match r.start_bound() {
+        std::ops::Bound::Excluded(&i) => i + 1,
+        std::ops::Bound::Included(&i) => i,
+        std::ops::Bound::Unbounded => 0,
+    };
+    let end = match r.end_bound() {
+        std::ops::Bound::Excluded(&i) => i,
+        std::ops::Bound::Included(&i) => i + 1,
+        std::ops::Bound::Unbounded => n,
+    };
+    (start, end)
+}
+trait Monoid {
+    fn id() -> Self;
+    fn op(&self, rhs: &Self) -> Self;
+}
+#[derive(Debug, Clone)]
+struct SegmentTree<M>
+where
+    M: Monoid,
+{
+    n: usize,
+    cap: usize,
+    values: Vec<M>,
+}
+#[allow(dead_code)]
+impl<M> SegmentTree<M>
+where
+    M: Monoid + Clone,
+{
+    fn new(n: usize) -> Self {
+        let cap = n.next_power_of_two();
+        SegmentTree {
+            n,
+            cap,
+            values: vec![M::id(); 2 * cap - 1],
+        }
+    }
+    fn with<T>(vals: &[T]) -> Self
+    where
+        T: Into<M> + Clone,
+    {
+        let n = vals.len();
+        let cap = n.next_power_of_two();
+        let mut values = Vec::with_capacity(2 * cap - 1);
+        values.resize(cap - 1, M::id());
+        values.extend(vals.iter().cloned().map(|x| x.into()));
+        values.resize(2 * cap - 1, M::id());
+        let mut st = SegmentTree { n, cap, values };
+        for idx in (0..cap - 1).rev() {
+            st.fix(idx);
+        }
+        st
+    }
+    fn fix(&mut self, idx: usize) {
+        let left_idx = 2 * (idx + 1) - 1;
+        let right_idx = 2 * (idx + 1);
+        if left_idx < self.values.len() {
+            self.values[idx] = M::op(&self.values[left_idx], &self.values[right_idx]);
+        }
+    }
+    fn fix_all(&mut self, mut idx: usize) {
+        while idx > 0 {
+            idx = (idx - 1) / 2;
+            self.fix(idx);
+        }
+    }
+    fn get(&self, pos: usize) -> M {
+        self.values[self.cap - 1 + pos].clone()
+    }
+    fn set<T>(&mut self, pos: usize, v: T)
+    where
+        T: Into<M>,
+    {
+        let idx = self.cap - 1 + pos;
+        self.values[idx] = v.into();
+        self.fix_all(idx);
+    }
+    fn query(&self, r: impl std::ops::RangeBounds<usize>) -> M {
+        let (a, b) = range(r, self.n);
+        let mut left = M::id();
+        let mut right = M::id();
+        let mut left_idx = a + self.cap - 1;
+        let mut right_idx = b + self.cap - 1;
+        while left_idx < right_idx {
+            if left_idx % 2 == 0 {
+                left = M::op(&left, &self.values[left_idx]);
+                left_idx += 1;
+            }
+            if right_idx % 2 == 0 {
+                right = M::op(&self.values[right_idx - 1], &right);
+                right_idx -= 1;
+            }
+            left_idx = left_idx >> 1;
+            right_idx = (right_idx - 1) >> 1;
+        }
+        M::op(&left, &right)
+    }
+    fn right_partition_point<F>(&self, a: usize, mut f: F) -> Option<usize>
+    where
+        F: FnMut(&M) -> bool,
+    {
+        assert!(a <= self.cap);
+        if !f(&M::id()) {
+            Some(a)
+        } else if a == self.cap {
+            None
+        } else {
+            let mut b = a;
+            let mut idx = ((b + self.cap) >> (b + self.cap).trailing_zeros()) - 1;
+            let mut len = 1 << (b + self.cap).trailing_zeros();
+            let mut val = M::id();
+            let mut val_next = M::op(&val, &self.values[idx]);
+            while f(&val_next) {
+                val = val_next;
+                b += len;
+                len <<= (idx + 2).trailing_zeros();
+                idx = ((idx + 2) >> (idx + 2).trailing_zeros()) - 1;
+                if idx == 0 {
+                    return None;
+                }
+                val_next = M::op(&val, &self.values[idx]);
+            }
+            idx = 2 * idx + 1;
+            len >>= 1;
+            while idx < self.values.len() {
+                val_next = M::op(&val, &self.values[idx]);
+                if f(&val_next) {
+                    val = val_next;
+                    b += len;
+                    idx += 1;
+                }
+                len >>= 1;
+                idx = 2 * idx + 1;
+            }
+            Some(b + 1)
+        }
+    }
+    fn left_partition_point<F>(&self, b: usize, mut f: F) -> usize
+    where
+        F: FnMut(&M) -> bool,
+    {
+        assert!(b <= self.cap);
+        if !f(&M::id()) {
+            b
+        } else if b == 0 {
+            0
+        } else {
+            let mut a = b;
+            let mut idx = (a + self.cap - 1) >> (!(a + self.cap - 1)).trailing_zeros();
+            let mut len = 1 << (!(a + self.cap - 1)).trailing_zeros();
+            if idx == 0 {
+                len = self.cap;
+            } else {
+                idx -= 1;
+            }
+            let mut val = M::id();
+            let mut val_next = M::op(&self.values[idx], &val);
+            while f(&val_next) {
+                val = val_next;
+                a -= len;
+                if idx == 0 || (idx + 1).is_power_of_two() {
+                    return 0;
+                }
+                len <<= (!idx).trailing_zeros();
+                idx >>= (!idx).trailing_zeros();
+                idx -= 1;
+                val_next = M::op(&self.values[idx], &val);
+            }
+            idx = 2 * idx + 2;
+            len >>= 1;
+            while idx < self.values.len() {
+                val_next = M::op(&self.values[idx], &val);
+                if f(&val_next) {
+                    val = val_next;
+                    a -= len;
+                    idx -= 1;
+                }
+                len >>= 1;
+                idx = 2 * idx + 2;
+            }
+            a
+        }
+    }
+}
+macro_rules! define_monoid {
+    ($ name : ident , $ t : ty , $ id : expr , $ op : expr ) => {
+        #[derive(Clone, Copy, Debug)]
+        struct $name($t);
+        impl Monoid for $name {
+            fn id() -> Self {
+                Self($id)
+            }
+            fn op(&self, rhs: &Self) -> Self {
+                Self(($op)(self.0, rhs.0))
+            }
+        }
+        impl From<$t> for $name {
+            fn from(x: $t) -> $name {
+                Self(x)
+            }
+        }
+    };
+}
+
+define_monoid!(Sum, usize, 0, std::ops::Add::add);
+
+fn main() {
+    let n = read::<usize>();
+    let s = read_str();
+    let q = read::<usize>();
+    let query = read_vec(q, || read_tuple!(usize, usize, String));
+
+    let t = (0..n)
+        .map(|i| i)
+        .coalesce(|i, j| {
+            assert!(i < j);
+            if s[j - 1] <= s[j] {
+                Ok(i)
+            } else {
+                Err((i, j))
+            }
+        })
+        .chain(once(n))
+        .collect::<BTreeSet<_>>();
+
+    let mut init = vec![SegmentTree::<Sum>::new(n); 26];
+    for (i, c) in s.citer().enumerate() {
+        init[c as usize - 'a' as usize].set(i, 1);
+    }
+
+    query
+        .iter()
+        .scan((s, t, init), |(s, t, sts), qq| {
+            if qq.0 == 1 {
+                let x = qq.1 - 1;
+                let c = qq.2.chars().next().unwrap();
+
+                let left0 = x > 0 && s[x - 1] <= s[x];
+                let right0 = x + 1 < n && s[x] <= s[x + 1];
+
+                sts[s[x] as usize - 'a' as usize].set(x, 0);
+                sts[c as usize - 'a' as usize].set(x, 1);
+                s[x] = c;
+
+                let left1 = x > 0 && s[x - 1] <= s[x];
+                let right1 = x + 1 < n && s[x] <= s[x + 1];
+
+                if left0 != left1 {
+                    if left1 {
+                        t.remove(&x);
+                    } else {
+                        t.insert(x);
+                    }
+                }
+
+                if right0 != right1 {
+                    if right1 {
+                        t.remove(&(x + 1));
+                    } else {
+                        t.insert(x + 1);
+                    }
+                }
+
+                Some(None)
+            } else {
+                let l = qq.1 - 1;
+                let r = qq.2.parse::<usize>().unwrap();
+
+                if t.range(l + 1..r).next().is_some() {
+                    return Some(Some(false));
+                }
+
+                let freq = sts.iter().map(|st| st.query(..).0).collect::<Vec<_>>();
+                let subfreq = sts.iter().map(|st| st.query(l..r).0).collect::<Vec<_>>();
+
+                let c0 = s[l] as usize - 'a' as usize;
+
+                let ok = once(subfreq[c0])
+                    .chain(freq[c0 + 1..].citer())
+                    .scan(0, |sum, x| {
+                        *sum += x;
+
+                        Some(min(x, (r - l).saturating_sub(*sum - x)))
+                    })
+                    .eq(subfreq[c0..].citer());
+
+                Some(Some(ok))
+            }
+        })
+        .flatten()
+        .for_each(|ans| {
+            if ans {
+                println!("Yes");
+            } else {
+                println!("No");
+            }
+        });
+}
