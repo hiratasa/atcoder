1d60084287efc8a40c9a0ece57159f42ad2576be
hiratasa <36002166+hiratasa@users.noreply.github.com>
2023-03-14 01:41:32 +0900

    Add codes

---
+++ arc158-rs/src/c.rs
@@ -0,0 +1,271 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::f64;
+#[allow(unused_imports)]
+use std::i64;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rand::{rngs::SmallRng, seq::IteratorRandom, seq::SliceRandom, Rng, SeedableRng};
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+
+// vec with some initial value
+#[allow(unused_macros)]
+macro_rules! vvec {
+    ($($x:expr),+; $y:expr; $n:expr) => {{
+        let mut v = vec![$y; $n];
+
+        let mut it = v.iter_mut();
+        $(
+            *it.next().unwrap() = $x;
+        )+
+
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! it {
+    ($x:expr) => {
+        once($x)
+    };
+    ($first:expr,$($x:expr),+) => {
+        chain(
+            once($first),
+            it!($($x),+)
+        )
+    };
+    ($($x:expr),+,) => {
+        it![$($x),+]
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        if $n > 0 {
+            bs.buffer_mut()[0] = $x as u64;
+        }
+        bs
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! pushed {
+    ($c:expr, $x:expr) => {{
+        let x = $x;
+        let mut c = $c;
+        c.push(x);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! inserted {
+    ($c:expr, $($x:expr),*) => {{
+        let mut c = $c;
+        c.insert($($x),*);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! read_tuple {
+    ($($t:ty),+) => {{
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let mut it = line.trim()
+            .split_whitespace();
+
+        ($(
+            it.next().unwrap().parse::<$t>().ok().unwrap()
+        ),+)
+    }}
+}
+
+#[allow(dead_code)]
+fn read<T: FromStr>() -> T {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    line.trim().to_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_str() -> Vec<char> {
+    read::<String>().chars().collect()
+}
+
+#[allow(dead_code)]
+fn read_digits() -> Vec<usize> {
+    read::<String>()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_row<T: FromStr>() -> Vec<T> {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+
+    line.trim()
+        .split_whitespace()
+        .map(|s| s.parse().ok().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_col<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read()).collect()
+}
+
+#[allow(dead_code)]
+fn read_mat<T: FromStr>(n: usize) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_row()).collect()
+}
+
+#[allow(dead_code)]
+fn read_vec<R, F: FnMut() -> R>(n: usize, mut f: F) -> Vec<R> {
+    (0..n).map(|_| f()).collect()
+}
+
+#[allow(dead_code)]
+fn println_opt<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(ans) = ans {
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
+}
+
+trait IterCopyExt<'a, T>: IntoIterator<Item = &'a T> + Sized
+where
+    T: 'a + Copy,
+{
+    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
+        self.into_iter().copied()
+    }
+}
+
+impl<'a, T, I> IterCopyExt<'a, T> for I
+where
+    I: IntoIterator<Item = &'a T>,
+    T: 'a + Copy,
+{
+}
+
+const D: usize = 16;
+
+fn solve0(a: &[usize]) -> usize {
+    iproduct!(a.citer(), a.citer())
+        .map(|(x, y)| {
+            iterate(x + y, |&z| z / 10)
+                .take_while(|&z| z > 0)
+                .map(|z| z % 10)
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+fn count_carry_chain(b: &[([u8; D], usize)], x0: &[u8], i: usize) -> usize {
+    if i >= 15 {
+        return 0;
+    }
+
+    let d = x0[i];
+
+    let idx0 = b
+        .binary_search_by(|(x, y)| x[i].cmp(&(9 - d)).then(Ordering::Greater))
+        .map_or_else(|idx| idx, |idx| idx);
+    let idx1 = b
+        .binary_search_by(|(x, y)| x[i].cmp(&(10 - d)).then(Ordering::Greater))
+        .map_or_else(|idx| idx, |idx| idx);
+
+    idx1 - idx0 + count_carry_chain(&b[idx0..idx1], x0, i + 1)
+}
+
+fn main() {
+    let n = read::<usize>();
+    let a = read_row::<usize>();
+
+    let mut b = a
+        .citer()
+        .map(|x| {
+            let mut v = [0; D];
+            for (i, d) in iterate(x, |&y| y / 10)
+                .take_while(|&y| y > 0)
+                .map(|y| (y % 10) as u8)
+                .enumerate()
+            {
+                v[i] = d;
+            }
+
+            let y = v.citer().fold(0, |a, b| a * 10 + b as usize);
+
+            (v, y)
+        })
+        .collect::<Vec<_>>();
+
+    let mut c = 0;
+    for i in 0..15 {
+        b.sort_by_key(|(x, _)| x[i]);
+        let mut b2 = b.clone();
+
+        let mut idx = 0;
+        let mut idx2 = n;
+        for j in 0..10 {
+            let prev = idx;
+            while idx < n && b[idx].0[i] == j {
+                idx += 1;
+            }
+            while idx2 > 0 && b[idx2 - 1].0[i] + j >= 10 {
+                idx2 -= 1;
+            }
+
+            c += (n - idx2) * (idx - prev);
+            // eprintln!("#i={}, j={}, c={}", i, j, c);
+
+            let d = 10usize.pow(15 - i as u32);
+            b2[idx2..].sort_by_key(|(_, x)| x % d);
+
+            for (x, _) in b[prev..idx].iter() {
+                c += count_carry_chain(&b2[idx2..], x, i + 1);
+            }
+            // eprintln!("##i={}, j={}, c={}", i, j, c);
+        }
+    }
+
+    let s = b
+        .iter()
+        .map(|(x, _)| x.citer().map_into::<usize>().sum::<usize>())
+        .sum::<usize>();
+
+    // eprintln!("s={}, c={}", s, c);
+
+    let ans = 2 * n * s - 9 * c;
+
+    println!("{}", ans);
+}
