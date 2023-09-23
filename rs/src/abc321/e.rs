f8c0572aeb703156195b365f3daa438a7bf27faa
hiratasa <36002166+hiratasa@users.noreply.github.com>
2023-09-23 22:57:30 +0900

    Add codes

---
+++ rs/src/abc321/e.rs
@@ -0,0 +1,188 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::f64;
+#[allow(unused_imports)]
+use std::i64;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use rand::{rngs::SmallRng, seq::IteratorRandom, seq::SliceRandom, Rng, SeedableRng};
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+
+#[allow(unused_imports)]
+use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+#[allow(unused_imports)]
+use proconio::source::{Readable, Source};
+
+// vec with some initial value
+#[allow(unused_macros)]
+macro_rules! vvec {
+    ($($x:expr),+; $y:expr; $n:expr) => {{
+        let mut v = vec![$y; $n];
+
+        let mut it = v.iter_mut();
+        $(
+            *it.next().unwrap() = $x;
+        )+
+
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! it {
+    ($x:expr) => {
+        once($x)
+    };
+    ($first:expr,$($x:expr),+) => {
+        chain(
+            once($first),
+            it!($($x),+)
+        )
+    };
+    ($($x:expr),+,) => {
+        it![$($x),+]
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! bitset {
+    ($n:expr, $x:expr) => {{
+        let mut bs = BitSet::new($n);
+        if $n > 0 {
+            bs.buffer_mut()[0] = $x as u64;
+        }
+        bs
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! pushed {
+    ($c:expr, $x:expr) => {{
+        let x = $x;
+        let mut c = $c;
+        c.push(x);
+        c
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! inserted {
+    ($c:expr, $($x:expr),*) => {{
+        let mut c = $c;
+        c.insert($($x),*);
+        c
+    }};
+}
+
+#[allow(dead_code)]
+fn println_opt<T: std::fmt::Display>(ans: Option<T>) {
+    if let Some(ans) = ans {
+        println!("{}", ans);
+    } else {
+        println!("-1");
+    }
+}
+
+use easy_ext::ext;
+
+#[ext(IterCopyExt)]
+impl<'a, I, T> I
+where
+    Self: IntoIterator<Item = &'a T>,
+    T: 'a + Copy,
+{
+    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
+        self.into_iter().copied()
+    }
+}
+
+enum Digits {}
+
+impl Readable for Digits {
+    type Output = Vec<usize>;
+    fn read<R: std::io::BufRead, S: Source<R>>(source: &mut S) -> Vec<usize> {
+        source
+            .next_token_unwrap()
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as usize)
+            .collect()
+    }
+}
+
+fn calc(n: usize, x: usize, from: usize, d: usize) -> usize {
+    if x > n {
+        return 0;
+    }
+
+    if d == 0 {
+        return 1;
+    }
+
+    if d > 1000 {
+        return 0;
+    }
+
+    // down
+    let down = if n < 2usize.saturating_pow(d as u32).saturating_mul(x) {
+        0
+    } else if n < (2usize.saturating_pow(d as u32).saturating_mul(x + 1) - 1) {
+        [2 * x, 2 * x + 1]
+            .into_iter()
+            .filter(|&c| c != from)
+            .map(|c| calc(n, c, x, d - 1))
+            .sum::<usize>()
+    } else {
+        [2 * x, 2 * x + 1]
+            .into_iter()
+            .filter(|&c| c != from)
+            .map(|_| 1 << (d - 1))
+            .sum::<usize>()
+    };
+
+    // up
+    let up = if from == x / 2 || x == 1 {
+        0
+    } else {
+        calc(n, x / 2, x, d - 1)
+    };
+
+    down + up
+}
+
+fn main() {
+    input! {
+        t: usize,
+        cases: [(usize, usize, usize); t]
+    }
+
+    cases
+        .citer()
+        .map(|(n, x, k)| calc(n, x, x, k))
+        .for_each(|ans| {
+            println!("{ans}");
+        });
+}
