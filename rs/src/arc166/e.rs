84ad0e840d24d1790b69683c4d5e0ea2936344aa
hiratasa <36002166+hiratasa@users.noreply.github.com>
2023-10-14 17:03:59 +0900

    Add codes

--- rs/src/arc166/e.rs
+++ rs/src/arc166/e.rs
@@ -133,4 +133,189 @@ impl Readable for Digits {
     }
 }
 
-fn main() {}
+fn gcd(a: usize, b: usize) -> usize {
+    if a == 0 {
+        b
+    } else {
+        gcd(b % a, a)
+    }
+}
+
+#[allow(dead_code)]
+fn lower_bound<T, F>(mut begin: T, mut end: T, epsilon: T, f: F) -> T
+where
+    T: std::marker::Copy
+        + std::ops::Add<T, Output = T>
+        + std::ops::Sub<T, Output = T>
+        + std::ops::Div<T, Output = T>
+        + std::cmp::PartialOrd<T>
+        + std::convert::TryFrom<i32>,
+    F: Fn(T) -> std::cmp::Ordering,
+{
+    let two = T::try_from(2).ok().unwrap();
+    while end - begin >= epsilon {
+        let mid = begin + (end - begin) / two;
+        match f(mid) {
+            std::cmp::Ordering::Less => {
+                begin = mid + epsilon;
+            }
+            _ => {
+                end = mid;
+            }
+        }
+    }
+    begin
+}
+#[allow(dead_code)]
+fn lower_bound_int<T, F>(begin: T, end: T, f: F) -> T
+where
+    T: std::marker::Copy
+        + std::ops::Add<T, Output = T>
+        + std::ops::Sub<T, Output = T>
+        + std::ops::Div<T, Output = T>
+        + std::cmp::PartialOrd<T>
+        + std::convert::TryFrom<i32>,
+    F: Fn(T) -> std::cmp::Ordering,
+{
+    lower_bound(begin, end, T::try_from(1).ok().unwrap(), f)
+}
+
+// calc sum[x=0 to n-1] floor((a+x*b)/c)
+#[allow(dead_code)]
+fn floor_sum(n: usize, mut a: usize, mut b: usize, c: usize) -> usize {
+    if n == 0 {
+        return 0;
+    }
+
+    let mut ret = 0;
+
+    // if c < 0 {
+    //     return floor_sum(n, -a, -b, -c);
+    // }
+
+    ret += a.div_euclid(c) * n;
+    a = a.rem_euclid(c);
+    // assert!(a >= 0);
+
+    ret += b.div_euclid(c) * n * (n - 1) / 2;
+    b = b.rem_euclid(c);
+    // assert!(b >= 0);
+
+    if b == 0 {
+        return ret;
+    }
+
+    let last = a + n * b;
+    ret += floor_sum(last / c, last % c, c, b);
+    ret
+}
+
+// 分数a/bの*上側*最良近似分数の列を一次式の形で求める
+// 返り値: ((c, d), (e, f), k) -> (c+e*i)/(d+f*i) (0<=i<=k)
+fn get_upper_best_rational_approximations(
+    a: usize,
+    b: usize,
+) -> Vec<((usize, usize), (usize, usize), usize)> {
+    let mut rationals = vec![];
+
+    let mut lower = (0, 1);
+    let mut upper = (1, 1);
+
+    // rationals.push((lower, upper, 0));
+    rationals.push((upper, lower, 0));
+
+    let bb = b / gcd(a, b);
+
+    let mut denom = 1;
+    while denom < bb {
+        if (lower.0 + upper.0) * b < (lower.1 + upper.1) * a {
+            let u = (lower.1 * a - lower.0 * b) / (upper.0 * b - upper.1 * a);
+
+            // rationals.push((lower, upper, u));
+
+            lower = (lower.0 + upper.0 * u, lower.1 + upper.1 * u);
+
+            denom = lower.1;
+        } else {
+            let u = (upper.0 * b - upper.1 * a) / (lower.1 * a - lower.0 * b);
+
+            rationals.push((upper, lower, u));
+
+            upper = (lower.0 * u + upper.0, lower.1 * u + upper.1);
+
+            denom = upper.1;
+        }
+    }
+
+    rationals
+}
+
+fn main() {
+    input! {
+        cases: [(usize, usize, usize)]
+    }
+
+    cases
+        .citer()
+        .map(|(n, a, b)| {
+            let g = gcd(a, b);
+            let aa = a / g;
+            let bb = b / g;
+
+            let k = lower_bound_int(1, 1 << 42, |k: usize| {
+                let na = k - 1;
+                let nb = (bb - 1 + k * aa - 1) / bb;
+                let m = na - nb;
+                m.cmp(&n).then(Ordering::Less)
+            }) - 1;
+
+            let len = a * k - 2;
+
+            let r = (bb - (bb - 1 + k * aa) % bb) % bb * g;
+
+            // bで割った余りがr以上になるaの倍数で最小のものを求める
+            // let x = lower_bound_int(0, bb, |x: usize| {
+            //     // x*a%bがr以上
+            //     // ⇔ floor((x*a+(b-c))/b) - floor(x*a/b) > 0
+
+            //     let ok = floor_sum(x + 1, b - r, a, b) - floor_sum(x + 1, 0, a, b) > 0;
+
+            //     if ok {
+            //         Ordering::Greater
+            //     } else {
+            //         Ordering::Less
+            //     }
+            // });
+
+            let best_rational_approximations = get_upper_best_rational_approximations(a, b);
+            let x = best_rational_approximations
+                .citer()
+                .find_map(|((c, d), (e, f), u)| {
+                    // y/x=(c+i*e)/(d+i*f) where 0<=i<=u
+
+                    // b - r >= (b-余り) = yb - xa = (cb-da) - i*(fa-eb)
+                    let det0 = c * b - d * a;
+                    let det1 = f * a - e * b;
+                    if det1 == 0 {
+                        if b - r >= det0 {
+                            Some((c, e))
+                        } else {
+                            None
+                        }
+                    } else if b - r + u * det1 >= det0 {
+                        let idx = (det0.saturating_sub(b - r) + det1 - 1) / det1;
+
+                        Some((c + idx * e, d + idx * f))
+                    } else {
+                        None
+                    }
+                })
+                .unwrap()
+                .1;
+
+            (x * a + 1, x * a + 1 + len)
+        })
+        .for_each(|ans| {
+            println!("{} {}", ans.0, ans.1);
+        });
+}
