983b5f4e1e56385851a03bcd408c39a123a97601
hiratasa <36002166+hiratasa@users.noreply.github.com>
2024-06-01 18:47:07 +0900

    Update template

--- rs/src/.template.rs
+++ rs/src/.template.rs
@@ -1,69 +1,23 @@
+fn main() {}
+
 #[allow(unused_imports)]
-use std::{cmp::*, collections::*, f64, i64, io, iter::*, mem::*, str::*, usize};
+use std::{
+    cmp::{max, min, Ordering, Reverse},
+    collections::{BTreeMap, BinaryHeap, HashMap, VecDeque},
+    iter::*,
+    mem::{replace, take},
+};
 
 #[allow(unused_imports)]
 use bitset_fixed::BitSet;
 #[allow(unused_imports)]
-use itertools::{chain, iproduct, iterate, izip, repeat_n, Itertools};
+use itertools::*;
 #[allow(unused_imports)]
 use itertools_num::ItertoolsNum;
 #[allow(unused_imports)]
-use rand::{rngs::SmallRng, seq::IteratorRandom, seq::SliceRandom, Rng, SeedableRng};
+use proconio::{
+    input, input_interactive,
+    marker::{Chars, Usize1},
+};
 #[allow(unused_imports)]
 use rustc_hash::{FxHashMap, FxHashSet};
-
-#[allow(unused_imports)]
-use proconio::{
-    input,
-    marker::{Bytes, Chars, Isize1, Usize1},
-};
-
-// vec with some initial value
-#[allow(unused_macros)]
-macro_rules! vvec {
-    ($($x:expr),+; $y:expr; $n:expr) => {{
-        let mut v = vec![$y; $n];
-
-        let mut it = v.iter_mut();
-        $(
-            *it.next().unwrap() = $x;
-        )+
-
-        v
-    }}
-}
-
-#[allow(unused_macros)]
-macro_rules! bitset {
-    ($n:expr, $x:expr) => {{
-        let mut bs = BitSet::new($n);
-        if $n > 0 {
-            bs.buffer_mut()[0] = $x as u64;
-        }
-        bs
-    }};
-}
-
-#[allow(dead_code)]
-fn println_opt<T: std::fmt::Display>(ans: Option<T>) {
-    if let Some(ans) = ans {
-        println!("{}", ans);
-    } else {
-        println!("-1");
-    }
-}
-
-use easy_ext::ext;
-
-#[ext(IterCopyExt)]
-impl<'a, I, T> I
-where
-    Self: IntoIterator<Item = &'a T>,
-    T: 'a + Copy,
-{
-    fn citer(self) -> std::iter::Copied<Self::IntoIter> {
-        self.into_iter().copied()
-    }
-}
-
-fn main() {}
