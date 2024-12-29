f7481cb4fba88282ed8594981388c026dfaca7a4
hiratasa <36002166+hiratasa@users.noreply.github.com>
2024-12-30 00:19:00 +0900

    Add codes

---
+++ rs/src/agc070/a.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let x = {
+        let mut c = 1;
+        let mut x = vec![];
+        let z = 102;
+        loop {
+            // println!("#{c}");
+            let r = c % 10;
+            x.push(r);
+            c /= 10;
+            c += r * z;
+            if c == 1 {
+                break;
+            }
+        }
+        x
+    };
+
+    let s = once(&x)
+        .cycle()
+        .flatten()
+        .take(5000)
+        .copied()
+        .collect::<Vec<_>>();
+
+    println!("{}", x.iter().rev().join(""));
+    println!("{}", s.iter().rev().join(""));
+}
+
+use std::collections::BTreeSet;
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Ordering, Reverse},
+    collections::{BTreeMap, BinaryHeap, HashMap, VecDeque},
+    iter::{once, once_with, repeat, repeat_with, successors},
+    mem::{replace, swap, take},
+};
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use proconio::{
+    input, input_interactive,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use rustc_hash::{FxHashMap, FxHashSet};
