f6e951cf056a54e18bdbc9d4ee3935a3aff35a80
hiratasa <36002166+hiratasa@users.noreply.github.com>
2025-05-04 20:20:45 +0900

    Add cods

---
+++ rs/src/abc404/c.rs
@@ -0,0 +1,113 @@
+fn main() {
+    input! {
+        n: usize, m: usize,
+        ab: [(Usize1, Usize1); m],
+    };
+
+    if n != m {
+        println!("No");
+        return;
+    }
+
+    let mut uf = ab.iter().fold(UnionFind::new(n), |mut uf, &(a, b)| {
+        uf.unite(a, b);
+        uf
+    });
+    if uf.size(0) != n {
+        println!("No");
+        return;
+    }
+
+    let degs = ab.iter().fold(vec![0; n], |mut degs, &(a, b)| {
+        degs[a] += 1;
+        degs[b] += 1;
+        degs
+    });
+    if degs.iter().any(|&d| d != 2) {
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
+}
+
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Ordering, Reverse},
+    collections::{BTreeMap, BinaryHeap, HashMap, VecDeque},
+    iter::{once, once_with, repeat, repeat_with, successors},
+    mem::{replace, swap, take},
+};
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use proconio::{
+    input, input_interactive,
+    marker::{Chars, Usize1},
+};
+#[allow(unused_imports)]
+use rustc_hash::{FxHashMap, FxHashSet};
+
+#[derive(Clone, Copy, Debug)]
+enum UnionFindNode {
+    Root { size: usize },
+    Child { parent: usize },
+}
+struct UnionFind {
+    g: Vec<UnionFindNode>,
+}
+#[allow(dead_code)]
+impl UnionFind {
+    fn new(n: usize) -> UnionFind {
+        use UnionFindNode::*;
+        UnionFind {
+            g: (0..n).map(|_| Root { size: 1 }).collect(),
+        }
+    }
+    fn root(&mut self, v: usize) -> usize {
+        use UnionFindNode::*;
+        let p = match self.g[v] {
+            Root { size: _ } => return v,
+            Child { parent: p } => p,
+        };
+        let r = self.root(p);
+        self.g[v] = Child { parent: r };
+        r
+    }
+    fn unite(&mut self, v: usize, u: usize) -> bool {
+        use UnionFindNode::*;
+        let rv = self.root(v);
+        let ru = self.root(u);
+        if rv == ru {
+            return false;
+        }
+        let size_rv = self.size(rv);
+        let size_ru = self.size(ru);
+        let (rsmall, rlarge) = if size_rv < size_ru {
+            (rv, ru)
+        } else {
+            (ru, rv)
+        };
+        self.g[rsmall] = Child { parent: rlarge };
+        self.g[rlarge] = Root {
+            size: size_rv + size_ru,
+        };
+        true
+    }
+    fn same(&mut self, v: usize, u: usize) -> bool {
+        self.root(v) == self.root(u)
+    }
+    fn size(&mut self, v: usize) -> usize {
+        use UnionFindNode::*;
+        let rv = self.root(v);
+        match self.g[rv] {
+            Root { size } => size,
+            Child { parent: _ } => unreachable!(),
+        }
+    }
+}
